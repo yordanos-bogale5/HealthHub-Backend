8272e1b56eb348fe9d32ad4708bc69119f8cecf7
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-05 15:57:25 +0300

    feat: Initialized .NET + Angular Environment

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthHub.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SampleController : ControllerBase
+{
+  [HttpGet]
+  public IActionResult Get()
+  {
+    return Ok("Hello World");
+  }
+}
