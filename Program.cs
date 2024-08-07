95aee5ce22a05e8db4aaf85999fd7ccdd5fcc7d7
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-07 23:20:20 +0300

    feat: Model Db using EFCore, Created Migrations, and Its working

--- Program.cs
+++ Program.cs
@@ -1,7 +1,23 @@
+using dotenv.net;
+using Microsoft.EntityFrameworkCore;
+
+// Load Environment Variables
+DotEnv.Load(options: new DotEnvOptions(ignoreExceptions: false));
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppContext>(options =>
+{
+    var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
+    if (string.IsNullOrEmpty(connectionString))
+    {
+        throw new InvalidOperationException("DB_CONNECTION environment variable is not set.");
+    }
+    Console.WriteLine($"This is the conn str: {connectionString}");
+    options.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
