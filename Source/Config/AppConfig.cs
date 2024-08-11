483ff3e6cfcc5af3a804ac2b659695516244b570
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-11 23:33:22 +0300

    auth0Authority config

--- Source/Config/AppConfig.cs
+++ Source/Config/AppConfig.cs
@@ -8,6 +8,7 @@ public class AppConfig(IConfiguration configuration)
   public string? MailSenderEmail { get; set; } = configuration["MAIL_SENDER_EMAIL"];
   public string? MailSenderPassword { get; set; } = configuration["MAIL_SENDER_PASSWORD"];
   public string? Auth0Domain { get; set; } = configuration["AUTH0_DOMAIN"];
+  public string? Auth0Authority { get; set; } = $"https://{configuration["AUTH0_DOMAIN"]}";
   public string? Auth0Audience { get; set; } = configuration["AUTH0_AUDIENCE"];
   public string? Auth0ClientId { get; set; } = configuration["AUTH0_CLIENT_ID"];
   public string? Auth0ClientSecret { get; set; } = configuration["AUTH0_CLIENT_SECRET"];
