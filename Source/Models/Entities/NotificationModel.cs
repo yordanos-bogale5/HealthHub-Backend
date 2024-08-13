636cb701bbf9f1e8a3fc09db3cbae9d146f16203
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-14 00:07:32 +0300

    feat: Notification Model

---
+++ Source/Models/Entities/NotificationModel.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using HealthHub.Source.Enums;
+
+namespace HealthHub.Source.Models.Entities;
+
+public class Notification
+{
+  public Guid NotificationId { get; set; } = Guid.NewGuid();
+  [Required]
+  public required Guid UserId { get; set; } // <<FK>>
+  [Required]
+  public required NotificationType NotificationType { get; set; }
+  [Required]
+  public required string Message { get; set; }
+
+  public virtual User? User { get; set; }
+
+  public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+  public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
