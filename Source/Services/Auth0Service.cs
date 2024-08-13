453afc239dfdd6ba229a60cca737f4d8d2e887a8
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-13 03:31:47 +0300

    feat: Login Auth0 Service

--- Source/Services/Auth0Service.cs
+++ Source/Services/Auth0Service.cs
@@ -1,8 +1,10 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Text.Json;
+using System.Text.Json.Nodes;
 using HealthHub.Source.Config;
 using HealthHub.Source.Models.Dtos;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Org.BouncyCastle.Asn1.Ocsp;
 using RestSharp;
 
 namespace HealthHub.Source.Services;
@@ -45,11 +47,11 @@ public class Auth0Service(AppConfig appConfig, ILogger<Auth0Service> logger)
 
     if (response.StatusCode != System.Net.HttpStatusCode.Created)
     {
-      logger.LogError(response.Content, $"Auth0 Create User Error");
+      logger.LogError(response.Content, $"Auth0 Create User Error\n\n");
       throw new Exception("Failed to create user in Auth0");
     }
 
-    logger.LogInformation($"Auth0 Create User Success:\n {response.Content}");
+    logger.LogInformation($"\n\nAuth0 Create User Success:\n {response.Content}");
 
 
     var userData = JsonSerializer.Deserialize<JsonElement>(response.Content!);
@@ -85,6 +87,53 @@ public class Auth0Service(AppConfig appConfig, ILogger<Auth0Service> logger)
     catch (System.Exception ex)
     {
       logger.LogError(ex, "Failed to delete user in Auth0");
+      throw;
+    }
+  }
+
+  public async Task<Auth0LoginDto?> LoginUserAsync(LoginUserDto loginUserDto)
+  {
+    try
+    {
+      var token = await GetManagementApiTokenAsync();
+
+      var client = new RestClient($"{appConfig.Auth0Authority}/oauth/token");
+
+      var request = new RestRequest() { Method = Method.Post };
+
+      request.AddHeader("content-type", "application/json");
+      request.AddHeader("Authorization", $"Bearer {token}");
+
+      request.AddBody(new
+      {
+        grant_type = "password",
+        username = loginUserDto.Email,
+        password = loginUserDto.Password,
+        audience = appConfig.Auth0Audience,
+        client_id = appConfig.Auth0ClientId,
+        client_secret = appConfig.Auth0ClientSecret
+      });
+
+      var response = await client.ExecuteAsync(request);
+
+      logger.LogInformation($"\n\nAuth0 Login User Response:\n {response.Content}");
+
+      var responseData = JsonSerializer.Deserialize<JsonElement>(response.Content!);
+
+      if (!response.IsSuccessStatusCode)
+      {
+        logger.LogError(response.Content);
+        throw new Exception(responseData.GetProperty("error_description").ToString());
+      }
+
+      return new Auth0LoginDto(
+        responseData.GetProperty("access_token").ToString(),
+        responseData.GetProperty("expires_in").GetInt32()
+      );
+    }
+    catch (System.Exception ex)
+    {
+      logger.LogError(ex, "Failed to login user in Auth0");
       throw;
     }
   }
@@ -129,7 +178,7 @@ public class Auth0Service(AppConfig appConfig, ILogger<Auth0Service> logger)
     var url = $"{appConfig.Auth0Authority}/oauth/token";
 
     var client = new RestClient(url);
-    logger.LogInformation($"Auth0 Get Management API Token Request:\n {url}");
+    // logger.LogInformation($"Auth0 Get Management API Token Request:\n {url}");
 
     var request = new RestRequest() { Method = Method.Post };
 
@@ -148,7 +197,6 @@ public class Auth0Service(AppConfig appConfig, ILogger<Auth0Service> logger)
       throw new Exception("Failed to get management API token");
     }
 
-    logger.LogInformation($"Auth0 Get Management API Token Success:\n {response.Content}");
 
     var tokenData = JsonSerializer.Deserialize<JsonElement>(response.Content!);
     return tokenData.GetProperty("access_token").GetString()!;
