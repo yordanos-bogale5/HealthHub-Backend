a4dcc7ccd1d7d0f5a83c32a17a50bdb9d510c4f0
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-14 00:08:19 +0300

    feat: Review Model

---
+++ Source/Models/Entities/ReviewModel..cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using HealthHub.Source.Attributes;
+
+namespace HealthHub.Source.Models.Entities;
+
+public class Review
+{
+  public Guid ReviewId { get; set; }
+  [Required]
+  public Guid PaymentId { get; set; } // <<FK>>
+  [Required]
+  [StarRating]
+  public decimal StarRating { get; set; }
+  [Required]
+  public required string ReviewText { get; set; }
+
+  public virtual Payment? Payment { get; set; }
+
+  public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+  public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
