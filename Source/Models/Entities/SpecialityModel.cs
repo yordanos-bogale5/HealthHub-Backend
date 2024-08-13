359ecbaf495f462e2731b3bc25c7721e2ad5e21c
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-14 00:08:30 +0300

    feat: Speciality Model

---
+++ Source/Models/Entities/SpecialityModel.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HealthHub.Source.Models.Entities;
+
+public class Speciality
+{
+  public Guid SpecialityId { get; set; }
+  [Required]
+  public required string SpecialityName { get; set; }
+  public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+  public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
