cfe6acfbaa65fb7a76936e024bf557d2a0f0fd5f
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-14 00:07:57 +0300

    feat: Patient Model

--- Source/Models/Entities/PatientModel.cs
+++ Source/Models/Entities/PatientModel.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HealthHub.Source.Models.Entities;
 
 public class Patient
 {
   public Guid PatientId { get; set; }
+
+  [Required]
+  public required Guid UserId { get; set; } // <<FK>>
+
   public string? MedicalHistory { get; set; }
   public string? EmergencyContactName { get; set; }
   public string? EmergencyContactPhone { get; set; }
+
+  public virtual User? User { get; set; }
 }
