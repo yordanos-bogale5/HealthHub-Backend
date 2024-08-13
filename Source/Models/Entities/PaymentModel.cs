7d868451d3477d6536805c1be6d0bbdb63ef0ef5
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-14 00:08:08 +0300

    feat: Payment Model

---
+++ Source/Models/Entities/PaymentModel.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using HealthHub.Source.Enums;
+
+namespace HealthHub.Source.Models.Entities;
+
+public class Payment
+{
+  public Guid PaymentId { get; set; } = Guid.NewGuid();
+
+  [Required]
+  public required Guid DoctorId { get; set; } // <<FK>>
+  [Required]
+  public required Guid PatientId { get; set; } // <<FK>>
+
+  [Required]
+  public required decimal Amount { get; set; }
+  [Required]
+  public required PaymentStatus PaymentStatus { get; set; }
+  [Required]
+  public required PaymentProvider PaymentProvider { get; set; } = PaymentProvider.Chapa;
+  public DateTime PaymentDate { get; set; } = DateTime.UtcNow;
+
+  public virtual Doctor? Doctor { get; set; } // <<NAV>>
+  public virtual Patient? Patient { get; set; } // <<NAV>>
+}
