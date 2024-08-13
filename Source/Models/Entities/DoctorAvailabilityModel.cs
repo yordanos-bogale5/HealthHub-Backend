705e1649bbfbbb709f996cf82d2aacef41f78546
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-14 00:05:09 +0300

    feat: Doctor Availability Model

---
+++ Source/Models/Entities/DoctorAvailabilityModel.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using HealthHub.Source.Enums;
+
+namespace HealthHub.Source.Models.Entities;
+
+public class DoctorAvailability
+{
+  public Guid AvailabilityId { get; set; }
+  [Required]
+  public Guid DoctorId { get; set; } // <<FK>>
+  [Required]
+  public Days AvailableDay { get; set; }
+  public TimeOnly StartTime { get; set; } = TimeOnly.MinValue.AddHours(6);
+  public TimeOnly EndTime { get; set; } = TimeOnly.MaxValue.AddHours(-6);
+
+  public virtual Doctor? Doctor { get; set; }
+
+  public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+  public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
