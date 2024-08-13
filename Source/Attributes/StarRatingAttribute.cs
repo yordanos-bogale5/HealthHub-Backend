6137147094c843f66e2d15c227e2f0c0565e76a1
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-14 00:03:17 +0300

    feat: Star Rating Validation Attribute

---
+++ Source/Attributes/StarRatingAttribute.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HealthHub.Source.Attributes;
+
+public class StarRatingAttribute : ValidationAttribute
+{
+  protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+  {
+    if (value is int || value is float || value is double || value is decimal)
+    {
+      if (!(0 <= (decimal)value && (decimal)value <= 5))
+      {
+        return new ValidationResult("Star rating must be between 0 - 5 inclusive");
+      }
+    }
+    else
+    {
+      return new ValidationResult("Star rating must be a number!");
+
+    }
+
+    return ValidationResult.Success;
+  }
+}
