e0c671df6a7d6708c154dc47610bf010348cbe99
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-14 00:04:30 +0300

    feat: Blog Like Model

---
+++ Source/Models/Entities/BlogLike.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HealthHub.Source.Models.Entities;
+
+public class BlogLike
+{
+  public Guid LikeId { get; set; } = Guid.NewGuid();
+  [Required]
+  public Guid UserId { get; set; } // <<FK>>
+  [Required]
+  public Guid BlogId { get; set; } // <<FK>>
+
+  public virtual User? User { get; set; }
+  public virtual Blog? Blog { get; set; }
+}
