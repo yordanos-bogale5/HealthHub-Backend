6023e3245f8b90a80e12c7cf78f348587f80dec8
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-14 00:05:32 +0300

    feat: File Model

---
+++ Source/Models/Entities/FileModel.cs
@@ -0,0 +1,8 @@
+namespace HealthHub.Source.Models.Entities;
+
+public class File
+{
+  public Guid FileId { get; set; } = Guid.NewGuid();
+  public required string MimeType { get; set; }
+  public required byte[] FileData { get; set; }
+}
