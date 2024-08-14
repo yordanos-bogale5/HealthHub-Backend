0aaf81f25f070ad0921307230010d47acaa9e1db
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-14 15:27:44 +0300

    feat: Doctor Service

---
+++ Source/Services/DoctorService.cs
@@ -0,0 +1,24 @@
+using HealthHub.Source.Data;
+using HealthHub.Source.Extensions;
+using HealthHub.Source.Models.Dtos;
+using HealthHub.Source.Models.Entities;
+
+public class DoctorService(ApplicationContext appContext, ILogger<DoctorService> logger)
+{
+  public async Task<Doctor?> CreateDoctor(CreateDoctorDto createDoctorDto)
+  {
+    try
+    {
+      var doctorResult = await appContext.Doctors.AddAsync(createDoctorDto.ToDoctor());
+      var doctor = doctorResult.Entity;
+
+      await appContext.SaveChangesAsync();
+      return doctor;
+    }
+    catch (Exception ex)
+    {
+      logger.LogError(ex, "Failed to Create Doctor");
+      throw;
+    }
+  }
+}
