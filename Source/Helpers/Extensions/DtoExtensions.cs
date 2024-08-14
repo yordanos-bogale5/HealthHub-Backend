276fddf6ff04844adb7cad2a8a688783aba20793
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-14 15:30:50 +0300

    feat: Added Helper Dto Extensions

--- Source/Helpers/Extensions/DtoExtensions.cs
+++ Source/Helpers/Extensions/DtoExtensions.cs
@@ -11,7 +11,7 @@ public static class DtoExtensions
   /// </summary>
   /// <param name="userDto"></param>
   /// <returns>User instance</returns>
-  public static User ToEntity(this RegisterUserDto userDto)
+  public static User ToUser(this RegisterUserDto userDto)
   {
     return new User()
     {
@@ -19,7 +19,57 @@ public static class DtoExtensions
       LastName = userDto.LastName,
       Email = userDto.Email,
       Phone = userDto.Phone,
-      Address = userDto.Address
+      Address = userDto.Address,
+      Gender = userDto.Gender,
+      Role = userDto.Role
+    };
+  }
+
+  /// <summary>
+  /// Maps CreatePatientDto to Patient
+  /// </summary>
+  /// <param name="createPatientDto"></param>
+  /// <returns></returns>
+  public static Patient ToPatient(this CreatePatientDto createPatientDto)
+  {
+    return new Patient()
+    {
+      UserId = createPatientDto.UserId,
+      MedicalHistory = createPatientDto.MedicalHistory,
+      EmergencyContactName = createPatientDto.EmergencyContactName,
+      EmergencyContactPhone = createPatientDto.EmergencyContactPhone
+    };
+  }
+
+  /// <summary>
+  /// Maps CreateDoctorDto to Doctor
+  /// </summary>
+  /// <param name="createDoctorDto"></param>
+  /// <returns></returns>
+  public static Doctor ToDoctor(this CreateDoctorDto createDoctorDto)
+  {
+    return new Doctor()
+    {
+      UserId = createDoctorDto.UserId,
+      SpecialityId = createDoctorDto.SpecialityId,
+      Qualifications = createDoctorDto.Qualifications,
+      Biography = createDoctorDto.Biography,
+      DoctorStatus = createDoctorDto.DoctorStatus
+    };
+  }
+
+
+  /// <summary>
+  /// Maps CreateAdminDto to Admin
+  /// </summary>
+  /// <param name="createAdminDto"></param>
+  /// <returns></returns>
+  public static Admin ToAdmin(this CreateAdminDto createAdminDto)
+  {
+    return new Admin()
+    {
+      UserId = createAdminDto.UserId
+      // More to be added here if admin changes in future, currently only UserId is needed
     };
   }
 }
