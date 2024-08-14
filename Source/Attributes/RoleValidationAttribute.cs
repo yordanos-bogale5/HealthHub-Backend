adad7c07750511cdba69f3b75011fd04ea293d70
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-14 09:45:31 +0300

    feat: Role Validation Attribute

---
+++ Source/Attributes/RoleValidationAttribute.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HealthHub.Source.Attributes;
+
+public class RoleValidationAttribute : ValidationAttribute
+{
+  protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+  {
+    if (value == null)
+    {
+      return new ValidationResult("Role is required.");
+    }
+
+    if (value.ToString() != "Admin" || value.ToString() != "Doctor" || value.ToString() != "Patient")
+    {
+      return new ValidationResult("Role be either Admin, Doctor, or Patient.");
+    }
+
+    return ValidationResult.Success;
+  }
+}
