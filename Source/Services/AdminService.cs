ceffab0c3169f7473e3a1c10c2af61c9dd1d94bf
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-14 15:25:57 +0300

    init: Admin Service

---
+++ Source/Services/AdminService.cs
@@ -0,0 +1,23 @@
+using HealthHub.Source.Data;
+using HealthHub.Source.Extensions;
+using HealthHub.Source.Models.Entities;
+
+public class AdminService(ILogger<AdminService> logger, ApplicationContext appContext)
+{
+  public async Task<Admin?> CreateAdmin(CreateAdminDto createAdminDto)
+  {
+    try
+    {
+      var adminResult = await appContext.Admins.AddAsync(createAdminDto.ToAdmin());
+      var admin = adminResult.Entity;
+
+      await appContext.SaveChangesAsync();
+      return admin;
+    }
+    catch (Exception ex)
+    {
+      logger.LogError(ex, "Failed to Create Admin");
+      throw;
+    }
+  }
+}
