a8fd771b046550620c80eed164a916e84a207a73
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-14 10:11:45 +0300

    feat: Not Past Date Attribute

---
+++ Source/Attributes/NotPastDateAttribute.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+public class NotPastDateAttribute : ValidationAttribute
+{
+  protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+  {
+    if (value == null)
+    {
+      return new ValidationResult("Date field must be provided");
+    }
+
+    if (value is not DateTime dateTime)
+    {
+      return new ValidationResult("Date should be in the correct format!");
+    }
+
+    // Compare the DateTime to the current UTC time
+    if (dateTime < DateTime.UtcNow)
+    {
+      return new ValidationResult("Date cannot be set to a past time.");
+    }
+
+    return ValidationResult.Success;
+  }
+}
