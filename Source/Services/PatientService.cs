6132398ec4e0b7546e876661b302cfd59e4b0737
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-14 15:27:33 +0300

    feat: Patient Service

---
+++ Source/Services/PatientService.cs
@@ -0,0 +1,25 @@
+using HealthHub.Source.Data;
+using HealthHub.Source.Extensions;
+using HealthHub.Source.Models.Dtos;
+using HealthHub.Source.Models.Entities;
+
+public class PatientService(ILogger<PatientService> logger, ApplicationContext appContext)
+{
+  public async Task<Patient?> CreatePatientAsync(CreatePatientDto createPatientDto)
+  {
+    try
+    {
+      var patientResult = await appContext.Patients.AddAsync(createPatientDto.ToPatient());
+      var patient = patientResult.Entity;
+
+      await appContext.SaveChangesAsync();
+
+      return patient;
+    }
+    catch (System.Exception ex)
+    {
+      logger.LogError(ex, "Failed to Create Patient");
+      throw;
+    }
+  }
+}
