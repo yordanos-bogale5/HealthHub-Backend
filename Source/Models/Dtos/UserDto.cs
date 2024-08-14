a81d953aad02fa789fbfbefafcd7048e1ec6da6e
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-14 13:01:59 +0300

    register dto change

--- Source/Models/Dtos/UserDto.cs
+++ Source/Models/Dtos/UserDto.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 using HealthHub.Source.Attributes;
 using HealthHub.Source.Enums;
 
@@ -39,28 +40,43 @@ public record ProfileDto(
 
 
 /// <summary>
-/// Register User Data Transfer Object. Sent from the client to the server for registering a new user. Used to validate client request upon hitting register endpoint.
+/// Register User DatatransferObject
 /// </summary>
-/// <param name="FirstName"></param>
-/// <param name="LastName"></param>
-/// <param name="Email"></param>
-/// <param name="Password"></param>
-/// <param name="Phone"></param>
-/// <param name="Gender"></param>
-/// <param name="DateOfBirth"></param>
-/// <param name="ProfilePicture"></param>
-/// <param name="Address"></param>
-public record RegisterUserDto(
-  [Required] string FirstName,
-  [Required] string LastName,
-  [Required][EmailAddress] string Email,
-  [Required][Password][PasswordPropertyText] string Password,
-  [Required][Phone][MinLength(4, ErrorMessage = "The field must be at least 4 characters long.")] string Phone,
-  [Required] Gender Gender,
-  [Required][AgeAbove18] DateTime DateOfBirth,
-  string? ProfilePicture,
-  [Required] string Address
-);
+public record RegisterUserDto
+{
+  [Required]
+  public required string FirstName { get; init; }
+
+  [Required]
+  public required string LastName { get; init; }
+
+  [Required]
+  [EmailAddress]
+  public required string Email { get; init; }
+
+  [Required]
+  [Password]
+  public required string Password { get; init; }
+
+  [Required]
+  [Phone]
+  [MinLength(4, ErrorMessage = "The field must be at least 4 characters long.")]
+  public required string Phone { get; init; }
+
+  [Gender]
+  public required Gender Gender { get; init; }
+
+  [Required]
+  [AgeAbove18]
+  public required DateTime DateOfBirth { get; init; }
+
+  [Required]
+  public required string Address { get; init; }
+
+  [Required]
+  [RoleValidation]
+  public required Role Role { get; init; }
+}
 
 /// <summary>
 /// These are the fields what a newly created Auth0 User will have
