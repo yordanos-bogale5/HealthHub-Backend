8313c9d2c2e6f20655da0d47fbc29fdb6a98c364
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-17 01:31:14 +0300

    feat: Error Constants for Easing Maintenance

---
+++ Source/Helpers/Constants/ErrorConstants.cs
@@ -0,0 +1,9 @@
+namespace HealthHub.Source.Helpers.Constants;
+
+public static class ErrorConstants
+{
+  public static string ModelValidationError = "Request Model Validation Error has occured!";
+  public static string ValidationError = "Request Validation Error has occured!";
+  public static string FluentValidationErrors = "FluentValidationErrors";
+  public static string ModelStateErrors = "ModelStateErrors";
+}
