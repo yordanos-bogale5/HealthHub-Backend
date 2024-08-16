10518fc0d92d3cf6b32aba93c666d661f40fb729
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-16 22:52:04 +0300

    feat: Custom Validation Middleware Extension

---
+++ Source/Helpers/Extensions/CustomValidationExtensions.cs
@@ -0,0 +1,11 @@
+using HealthHub.Source.Middlewares;
+
+namespace HealthHub.Source.Helpers.Extensions;
+
+public static class CustomValidationMiddlewareExtensions
+{
+  public static IApplicationBuilder UseCustomValidation(this IApplicationBuilder applicationBuilder)
+  {
+    return applicationBuilder.UseMiddleware<CustomValidationMiddleware>();
+  }
+}
