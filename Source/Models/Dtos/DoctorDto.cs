786a58ee939b71c587de985801f7a30648f84fa6
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-17 14:29:28 +0300

    Delete: SpecialityId from Doctor Model

--- Source/Models/Dtos/DoctorDto.cs
+++ Source/Models/Dtos/DoctorDto.cs
@@ -5,8 +5,8 @@ namespace HealthHub.Source.Models.Dtos;
 public record CreateDoctorDto
 {
   public required Guid UserId { get; init; }
-  public required Guid SpecialityId { get; init; }
   public required string Qualifications { get; init; }
   public required string Biography { get; init; }
   public DoctorStatus DoctorStatus { get; init; } = DoctorStatus.Active;
+
 }
