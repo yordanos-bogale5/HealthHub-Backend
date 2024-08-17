1561a48421eb42f49e63070cf81314c6fbdd6354
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-17 14:29:47 +0300

    Add: DoctorId in Speciality Model

--- Source/Models/Entities/SpecialityModel.cs
+++ Source/Models/Entities/SpecialityModel.cs
@@ -6,7 +6,12 @@ public class Speciality
 {
   public Guid SpecialityId { get; set; }
   [Required]
+  public required Guid DoctorId { get; set; } // <<FK>>
+  [Required]
   public required string SpecialityName { get; set; }
+
+  public virtual Doctor? Doctor { get; set; } // Nav
+
   public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
   public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
