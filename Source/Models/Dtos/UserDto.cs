b6bd068ca0642a70b567745c971d3780b8741c6c
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-17 21:09:21 +0300

    feat: DoctorUser Dto For Joined Payload

--- Source/Models/Dtos/UserDto.cs
+++ Source/Models/Dtos/UserDto.cs
@@ -94,6 +94,44 @@ public record RegisterUserDto
 }
 
 /// <summary>
+/// A User with Doctor properties joined
+/// </summary>
+public record DoctorUser
+{
+  [Required]
+  public required string FirstName { get; init; }
+
+  [Required]
+  public required string LastName { get; init; }
+
+  [Required]
+  [EmailAddress]
+  public required string Email { get; init; }
+
+  [Required]
+  [Phone]
+  [MinLength(4, ErrorMessage = "The field must be at least 4 characters long.")]
+  public required string Phone { get; init; }
+
+  [Required]
+  public required Gender Gender { get; init; }
+
+  [Required]
+  public required DateTime DateOfBirth { get; init; }
+
+  [Required]
+  public required string Address { get; init; }
+
+  // If the user is a doctor, they may/may-not specify the following
+  // Note the following fields MUST be validated in the controller based on the Role field provided as payload
+  public List<string> Specialities { get; set; } = [];
+  public string? Qualifications { get; set; }
+  public string? Biography { get; set; }
+  public DoctorStatus DoctorStatus { get; set; } = DoctorStatus.Active;
+}
+
+
+/// <summary>
 /// These are the fields what a newly created Auth0 User will have
 /// </summary>
 /// <param name="UserId"></param>
