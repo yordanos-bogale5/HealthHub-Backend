10f107e003db0b211bd5598324e155d719d3850a
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-17 14:32:34 +0300

    feat: CreateSpecialityAsync and CreateSpecialitiesAsync Done

--- Source/Services/SpecialityService.cs
+++ Source/Services/SpecialityService.cs
@@ -1,21 +1,43 @@
 using HealthHub.Source.Data;
+using HealthHub.Source.Helpers.Extensions;
 using HealthHub.Source.Models.Dtos;
 using HealthHub.Source.Models.Entities;
 
 public class SpecialityService(ApplicationContext appContext, ILogger<SpecialityService> logger)
 {
-  public async Task<Speciality?> CreateSpeciality(SpecialityDto specialityDto)
+  public async Task<Speciality?> CreateSpecialityAsync(CreateSpecialityDto specialityDto)
   {
     try
     {
-
-      // TODO
-
-      return null;
+      var speciality = await appContext.Specialities.AddAsync(specialityDto.ToSpeciality());
+      await appContext.SaveChangesAsync();
+      return speciality.Entity;
     }
     catch (System.Exception ex)
     {
       logger.LogError(ex, "Error creating Speciality!");
+      throw new Exception("Error creating Speciality!");
+    }
+  }
+
+  public async Task<List<Speciality>?> CreateSpecialitiesAsync(List<CreateSpecialityDto> specialityDtos)
+  {
+    try
+    {
+      List<Speciality> createResult = [];
+      foreach (CreateSpecialityDto specialityDto in specialityDtos)
+      {
+        var specialityResult = await CreateSpecialityAsync(specialityDto);
+        if (specialityResult != null)
+        {
+          createResult.Add(specialityResult);
+        }
+      }
+      return createResult;
+    }
+    catch (System.Exception ex)
+    {
+      logger.LogError($"Error Creating Specialities {ex}");
       throw;
     }
   }
