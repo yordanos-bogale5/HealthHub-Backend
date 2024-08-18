75b9b7ed6807cfa212ec38d365839a5198eeca36
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-18 21:23:02 +0300

    Register DoctorSpeciality Service

--- Program.cs
+++ Program.cs
@@ -1,33 +1,32 @@
-using FluentValidation;
 using System.Text.Json.Serialization;
 using dotenv.net;
+using FluentValidation;
 using HealthHub.Source.Config;
 using HealthHub.Source.Data;
+using HealthHub.Source.Filters.Error;
+using HealthHub.Source.Helpers.Extensions;
 using HealthHub.Source.Services;
+using HealthHub.Source.Validation.UserValidation;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Serilog;
-using HealthHub.Source.Validation.UserValidation;
-using HealthHub.Source.Helpers.Extensions;
-using HealthHub.Source.Filters.Error;
-using Microsoft.AspNetCore.Mvc;
-
 
 // Load Environment Variables
 DotEnv.Load(options: new DotEnvOptions(ignoreExceptions: false));
 
 var builder = WebApplication.CreateBuilder(args);
+
+
 {
-
-
     // Configure Serilog with appropriate sinks
     Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Debug() // Set the minimum log level to Debug
-            .WriteTo.Console() // Write logs to the console
-            .WriteTo.File("Logs/HealthHub.log", rollingInterval: RollingInterval.Day) // Write logs to a file
-            .WriteTo.Seq("http://localhost:5341/") // Write logs to Seq
-            .CreateLogger();
+        .MinimumLevel.Debug() // Set the minimum log level to Debug
+        .WriteTo.Console() // Write logs to the console
+        .WriteTo.File("Logs/HealthHub.log", rollingInterval: RollingInterval.Day) // Write logs to a file
+        .WriteTo.Seq("http://localhost:5341/") // Write logs to Seq
+        .CreateLogger();
 
     Log.Information("Application Starting...");
 
@@ -46,35 +45,35 @@ var builder = WebApplication.CreateBuilder(args);
     // Configure authentication with JWT and Auth0
     // 1. Set JwtBearer as the default authentication and challenge schemes
     // 2. Configure JwtBearer options with Auth0 settings
-    builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-
-    })
-    .AddJwtBearer(options =>
-    {
-        var appConfig = new AppConfig(builder.Configuration);
-        options.Authority = $"https://{appConfig.Auth0Domain}/";
-        options.Audience = appConfig.Auth0Audience;
-        options.RequireHttpsMetadata = appConfig.IsProduction ?? false;
-
-        Log.Logger.Information($"\nAudience: {options.Audience}");
-        Log.Logger.Information($"\nAuthority: {options.Authority}");
-        Log.Logger.Information($"\nClientId: {appConfig.Auth0ClientId}");
-        Log.Logger.Information($"\nClientSecret: {appConfig.Auth0ClientSecret}");
-
-        // Configure Token Validation Parameters
-        options.TokenValidationParameters = new TokenValidationParameters
+    builder
+        .Services.AddAuthentication(options =>
         {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = appConfig.Auth0Authority,
-            ValidAudience = appConfig.Auth0Audience
-        };
-    });
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            var appConfig = new AppConfig(builder.Configuration);
+            options.Authority = $"https://{appConfig.Auth0Domain}/";
+            options.Audience = appConfig.Auth0Audience;
+            options.RequireHttpsMetadata = appConfig.IsProduction ?? false;
+
+            Log.Logger.Information($"\nAudience: {options.Audience}");
+            Log.Logger.Information($"\nAuthority: {options.Authority}");
+            Log.Logger.Information($"\nClientId: {appConfig.Auth0ClientId}");
+            Log.Logger.Information($"\nClientSecret: {appConfig.Auth0ClientSecret}");
+
+            // Configure Token Validation Parameters
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = appConfig.Auth0Authority,
+                ValidAudience = appConfig.Auth0Audience
+            };
+        });
 
     // Configure Authorization
     builder.Services.AddAuthorization(options =>
@@ -101,19 +100,22 @@ var builder = WebApplication.CreateBuilder(args);
         return new AppConfig(config);
     });
 
-
     // Database Service
-    builder.Services.AddDbContext<ApplicationContext>((serviceProvider, options) =>
-    {
-        var appConfig = serviceProvider.GetRequiredService<AppConfig>();
-        var connectionString = appConfig.DatabaseConnection;
-        if (string.IsNullOrEmpty(connectionString))
+    builder.Services.AddDbContext<ApplicationContext>(
+        (serviceProvider, options) =>
         {
-            throw new InvalidOperationException("DB_CONNECTION environment variable is not set.");
+            var appConfig = serviceProvider.GetRequiredService<AppConfig>();
+            var connectionString = appConfig.DatabaseConnection;
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException(
+                    "DB_CONNECTION environment variable is not set."
+                );
+            }
+            Log.Information($"This is the conn str: {connectionString}");
+            options.UseSqlServer(connectionString);
         }
-        Log.Information($"This is the conn str: {connectionString}");
-        options.UseSqlServer(connectionString);
-    });
+    );
 
     // Register Services
     builder.Services.AddTransient<UserService>();
@@ -122,6 +124,7 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddTransient<AdminService>();
 
     builder.Services.AddTransient<SpecialityService>();
+    builder.Services.AddTransient<DoctorSpecialityService>();
 
     builder.Services.AddTransient<AuthService>();
     builder.Services.AddTransient<Auth0Service>();
@@ -130,18 +133,18 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddTransient<FileService>();
     builder.Services.AddTransient<RenderingService>();
 
-
-
-    builder.Services.AddControllers()
-    .AddNewtonsoftJson(options =>
-    {
-        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-        options.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
-    });
-
-
-
-
+    builder
+        .Services.AddControllers()
+        .AddNewtonsoftJson(options =>
+        {
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft
+                .Json
+                .ReferenceLoopHandling
+                .Ignore;
+            options.SerializerSettings.Converters.Add(
+                new Newtonsoft.Json.Converters.StringEnumConverter()
+            );
+        });
 
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen(options =>
@@ -151,7 +154,6 @@ var builder = WebApplication.CreateBuilder(args);
         options.IncludeXmlComments(xmlPath);
     });
 
-
     // Close and Flush Serilog when the application exits
     AppDomain.CurrentDomain.ProcessExit += (s, e) => Log.CloseAndFlush();
 
@@ -159,8 +161,9 @@ var builder = WebApplication.CreateBuilder(args);
 }
 
 var app = builder.Build();
-{
 
+
+{
     app.UseSerilogRequestLogging(); // Enable Serilog Request Logging
 
     // app.UseExceptionHandler("/error"); // Exception handling endpoint
@@ -171,7 +174,6 @@ var app = builder.Build();
     app.UseAuthorization();
 
     app.MapControllers();
-
 
     if (app.Environment.IsDevelopment())
     {
