02e8cafc830aa852b2710d1681ed6db5a6d8b6aa
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-18 21:20:46 +0300

    Attributes

--- Source/Attributes/GenderAttribute.cs
+++ Source/Attributes/GenderAttribute.cs
@@ -3,30 +3,31 @@ using HealthHub.Source.Models.Enums;
 
 public class GenderAttribute : ValidationAttribute
 {
-  protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
-  {
-    try
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
-      if (value == null)
-      {
-        return new ValidationResult("Gender field cannot be empty.");
-      }
+        try
+        {
+            if (value == null)
+            {
+                return new ValidationResult("Gender field cannot be empty.");
+            }
 
-      if (value is not Gender gender)
-      {
-        return new ValidationResult("Invalid gender value.");
-      }
+            if (value is not Gender gender)
+            {
+                return new ValidationResult("Invalid gender value.");
+            }
 
-      if (!Enum.IsDefined(typeof(Gender), gender))
-      {
-        return new ValidationResult("Gender must be either Male or Female.");
-      }
+            if (!Enum.IsDefined(typeof(Gender), gender))
+            {
+                return new ValidationResult("Gender must be either Male or Female.");
+            }
 
-      return ValidationResult.Success;
+            return ValidationResult.Success;
+        }
+        catch (System.Exception ex)
+        {
+            Console.WriteLine($"{ex}");
+            return new ValidationResult("Invalid Gender type! Must be either Male or Female!");
+        }
     }
-    catch (System.Exception ex)
-    {
-      return new ValidationResult("Invalid Gender type! Must be either Male or Female!");
-    }
-  }
 }
