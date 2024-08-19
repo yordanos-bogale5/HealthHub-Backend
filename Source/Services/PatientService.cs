7ee2fac4c911e968769783f9e53d84ee97a8ddd7
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-19 18:58:09 +0300

    Feat: Get All Patients Service

--- Source/Services/PatientService.cs
+++ Source/Services/PatientService.cs
@@ -2,6 +2,8 @@ using HealthHub.Source.Data;
 using HealthHub.Source.Helpers.Extensions;
 using HealthHub.Source.Models.Dtos;
 using HealthHub.Source.Models.Entities;
+using HealthHub.Source.Models.Responses;
+using Microsoft.EntityFrameworkCore;
 
 public class PatientService(ILogger<PatientService> logger, ApplicationContext appContext)
 {
@@ -20,6 +22,34 @@ public class PatientService(ILogger<PatientService> logger, ApplicationContext a
         {
             logger.LogError(ex, "Failed to Create Patient");
             throw new Exception("Failed to Create Patient");
+        }
+    }
+
+    /// <summary>
+    /// Gets all patients from the database
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
+    public async Task<ServiceResponse<List<PatientDto>>> GetAllPatientsAsync()
+    {
+        try
+        {
+            var patients = await appContext
+                .Patients.Include(p => p.User)
+                .Select(p => p.ToPatientDto())
+                .ToListAsync();
+
+            return new ServiceResponse<List<PatientDto>>
+            {
+                Data = patients,
+                Message = "Fetched all patients",
+                StatusCode = 200,
+                Success = true
+            };
+        }
+        catch (System.Exception ex)
+        {
+            throw new Exception("Failed to get all patients");
         }
     }
 }
