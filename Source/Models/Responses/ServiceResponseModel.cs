39ec40f96e203ee6975610f2e604fbfb113b2a3e
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-19 19:00:25 +0300

    Feat: Added Property Accessors

--- Source/Models/Responses/ServiceResponseModel.cs
+++ Source/Models/Responses/ServiceResponseModel.cs
@@ -1,30 +1,35 @@
 namespace HealthHub.Source.Models.Responses;
 
-
 public interface IServiceResponse
 {
-  public bool Success { get; }
-  public int StatusCode { get; }
-  public string? Message { get; }
+    public bool Success { get; set; }
+    public int StatusCode { get; set; }
+    public string? Message { get; set; }
 }
 
 public interface IServiceResponse<T> : IServiceResponse
 {
-  T? Data { get; }
+    T? Data { get; }
 }
 
-public class ServiceResponse(bool Success, int StatusCode, string? Message = null) : IServiceResponse
+public class ServiceResponse(bool Success, int StatusCode, string? Message = null)
+    : IServiceResponse
 {
-  public bool Success { get; } = Success;
-  public string? Message { get; } = Message;
-  public int StatusCode { get; } = StatusCode;
+    public bool Success { get; set; } = Success;
+    public string? Message { get; set; } = Message;
+    public int StatusCode { get; set; } = StatusCode;
 }
 
 public class ServiceResponse<T> : ServiceResponse, IServiceResponse<T>
 {
-  public T? Data { get; }
-  public ServiceResponse(bool Success, int StatusCode, T? Data, string? Message = null) : base(Success, StatusCode, Message)
-  {
-    this.Data = Data;
-  }
+    public T? Data { get; set; }
+
+    public ServiceResponse()
+        : base(true, 200) { }
+
+    public ServiceResponse(bool Success, int StatusCode, T? Data, string? Message = null)
+        : base(Success, StatusCode, Message)
+    {
+        this.Data = Data;
+    }
 }
