7d1e786893bd15882efeaacee55bb273ab606c23
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-19 18:32:29 +0300

    Feat: Patient Dto

--- Source/Models/Dtos/PatientDto.cs
+++ Source/Models/Dtos/PatientDto.cs
@@ -1,4 +1,5 @@
 using HealthHub.Source.Models.Entities;
+using HealthHub.Source.Models.Enums;
 
 namespace HealthHub.Source.Models.Dtos;
 
@@ -8,4 +9,23 @@ public record CreatePatientDto
     public string? MedicalHistory { get; init; }
     public string? EmergencyContactName { get; init; }
     public string? EmergencyContactPhone { get; init; }
+}
+
+public record PatientDto
+{
+    public required Guid UserId { get; init; }
+    public required Guid PatientId { get; init; }
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public required string Email { get; init; }
+    public required bool IsEmailVerified { get; set; }
+    public required string Phone { get; init; }
+    public required Gender Gender { get; init; }
+    public required DateTime DateOfBirth { get; init; }
+    public required string ProfilePicture { get; set; }
+    public required string Address { get; init; }
+
+    public required string MedicalHistory { get; init; }
+    public required string EmergencyContactName { get; init; }
+    public required string EmergencyContactPhone { get; init; }
 }
