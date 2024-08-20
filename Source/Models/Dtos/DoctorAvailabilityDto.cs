b69f1a2d0ada0965779953660549fb8527c20d6a
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-20 19:19:29 +0300

    Feat: Added Availability DTO

--- Source/Models/Dtos/DoctorAvailabilityDto.cs
+++ Source/Models/Dtos/DoctorAvailabilityDto.cs
@@ -1,9 +1,25 @@
 using HealthHub.Source.Models.Enums;
+using Microsoft.AspNetCore.Mvc;
 
-public record DoctorAvailabilityDto {
+public record DoctorAvailabilityDto
+{
   public required Guid DoctorAvailabilityId { get; set; }
   public required Guid DoctorId { get; set; }
   public required Days AvailableDay { get; set; }
   public required TimeOnly StartTime { get; set; }
   public required TimeOnly EndTime { get; set; }
+}
+
+public record Availability
+{
+  public required string Day { get; init; }
+  public required string StartTime { get; init; }
+  public required string EndTime { get; init; }
+
+  public void Deconstruct(out string day, out string startTime, out string endTime)
+  {
+    day = Day;
+    startTime = StartTime;
+    endTime = EndTime;
+  }
 }
