e986ac1114cd67512b310ae5c8f9b4bd8b802dde
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-20 18:02:02 +0300

    Responses

--- Source/Models/Responses/ApiResponseModel.cs
+++ Source/Models/Responses/ApiResponseModel.cs
@@ -1,28 +1,24 @@
 namespace HealthHub.Source.Models.Responses;
 
-public interface IApiResponse<T>
-{
+public interface IApiResponse<T> {
   bool Success { get; set; }
   string? Message { get; set; }
   T? Data { get; set; }
 }
 
-public class ApiResponse<T> : IApiResponse<T>
-{
+public class ApiResponse<T> : IApiResponse<T> {
   public bool Success { get; set; }
   public string? Message { get; set; }
   public T? Data { get; set; }
 
-  public ApiResponse(bool success, string? message, T? data)
-  {
+  public ApiResponse(bool success, string? message, T? data) {
     Success = success;
     Message = message;
     Data = data;
   }
 }
 
-public class ErrorResponse
-{
+public class ErrorResponse {
   public string title { get; set; } = "";
   public object errors { get; set; } = new { };
 }
--- Source/Models/Responses/ServiceResponseModel.cs
+++ Source/Models/Responses/ServiceResponseModel.cs
@@ -1,35 +1,30 @@
 namespace HealthHub.Source.Models.Responses;
 
-public interface IServiceResponse
-{
-    public bool Success { get; set; }
-    public int StatusCode { get; set; }
-    public string? Message { get; set; }
+public interface IServiceResponse {
+  public bool Success { get; set; }
+  public int StatusCode { get; set; }
+  public string? Message { get; set; }
 }
 
-public interface IServiceResponse<T> : IServiceResponse
-{
-    T? Data { get; }
+public interface IServiceResponse<T> : IServiceResponse {
+  T? Data { get; }
 }
 
 public class ServiceResponse(bool Success, int StatusCode, string? Message = null)
-    : IServiceResponse
-{
-    public bool Success { get; set; } = Success;
-    public string? Message { get; set; } = Message;
-    public int StatusCode { get; set; } = StatusCode;
+    : IServiceResponse {
+  public bool Success { get; set; } = Success;
+  public string? Message { get; set; } = Message;
+  public int StatusCode { get; set; } = StatusCode;
 }
 
-public class ServiceResponse<T> : ServiceResponse, IServiceResponse<T>
-{
-    public T? Data { get; set; }
+public class ServiceResponse<T> : ServiceResponse, IServiceResponse<T> {
+  public T? Data { get; set; }
 
-    public ServiceResponse()
-        : base(true, 200) { }
+  public ServiceResponse()
+      : base(true, 200) { }
 
-    public ServiceResponse(bool Success, int StatusCode, T? Data, string? Message = null)
-        : base(Success, StatusCode, Message)
-    {
-        this.Data = Data;
-    }
+  public ServiceResponse(bool Success, int StatusCode, T? Data, string? Message = null)
+      : base(Success, StatusCode, Message) {
+    this.Data = Data;
+  }
 }
