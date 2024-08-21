334ace4932db1acab31353f3788da5975bce29a9
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-21 14:27:11 +0300

    Feat: Get Patient Async By PatientId

--- Source/Services/PatientService.cs
+++ Source/Services/PatientService.cs
@@ -5,16 +5,21 @@ using HealthHub.Source.Models.Entities;
 using HealthHub.Source.Models.Responses;
 using Microsoft.EntityFrameworkCore;
 
-public class PatientService(ILogger<PatientService> logger, ApplicationContext appContext) {
-  public async Task<Patient?> CreatePatientAsync(CreatePatientDto createPatientDto) {
-    try {
+public class PatientService(ILogger<PatientService> logger, ApplicationContext appContext)
+{
+  public async Task<Patient?> CreatePatientAsync(CreatePatientDto createPatientDto)
+  {
+    try
+    {
       var patientResult = await appContext.Patients.AddAsync(createPatientDto.ToPatient());
       var patient = patientResult.Entity;
 
       await appContext.SaveChangesAsync();
 
       return patient;
-    } catch (System.Exception ex) {
+    }
+    catch (System.Exception ex)
+    {
       logger.LogError(ex, "Failed to Create Patient");
       throw new Exception("Failed to Create Patient");
     }
@@ -25,21 +30,52 @@ public class PatientService(ILogger<PatientService> logger, ApplicationContext a
   /// </summary>
   /// <returns></returns>
   /// <exception cref="Exception"></exception>
-  public async Task<ServiceResponse<List<PatientDto>>> GetAllPatientsAsync() {
-    try {
+  public async Task<ServiceResponse<List<PatientDto>>> GetAllPatientsAsync()
+  {
+    try
+    {
       var patients = await appContext
-          .Patients.Include(p => p.User)
-          .Select(p => p.ToPatientDto())
-          .ToListAsync();
+        .Patients.Include(p => p.User)
+        .Select(p => p.ToPatientDto())
+        .ToListAsync();
 
-      return new ServiceResponse<List<PatientDto>> {
+      return new ServiceResponse<List<PatientDto>>
+      {
         Data = patients,
         Message = "Fetched all patients",
         StatusCode = 200,
         Success = true
       };
-    } catch (System.Exception ex) {
+    }
+    catch (System.Exception ex)
+    {
       throw new Exception("Failed to get all patients");
+    }
+  }
+
+  public async Task<ServiceResponse<Patient>> GetPatientAsync(Guid patientId)
+  {
+    try
+    {
+      var patient = await appContext
+        .Patients.Include(p => p.User)
+        .SingleOrDefaultAsync(p => p.PatientId == patientId);
+      if (patient == null)
+      {
+        return new ServiceResponse<Patient>
+        {
+          Data = null,
+          Message = "Patient Not Found.",
+          StatusCode = 404,
+          Success = false
+        };
+      }
+      return new ServiceResponse<Patient>(true, 200, patient, "Patient found.");
+    }
+    catch (System.Exception ex)
+    {
+      logger.LogError(ex, "Failed to Get Patient");
+      throw;
     }
   }
 }
