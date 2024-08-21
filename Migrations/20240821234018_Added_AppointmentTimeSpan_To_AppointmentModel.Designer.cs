f653d5a98966c07bb97a28b6e87b56ba0c2edf2a
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-22 02:58:42 +0300

    Migrations

--- Migrations/20240821234018_Added_AppointmentTimeSpan_To_AppointmentModel.Designer.cs
+++ Migrations/20240821234018_Added_AppointmentTimeSpan_To_AppointmentModel.Designer.cs
@@ -4,6 +4,7 @@ using HealthHub.Source.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HealthHub.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20240821234018_Added_AppointmentTimeSpan_To_AppointmentModel")]
+    partial class Added_AppointmentTimeSpan_To_AppointmentModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240821234848_Added_Default30MinTimeSpan_ForAppointmentModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_Default30MinTimeSpan_ForAppointmentModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
