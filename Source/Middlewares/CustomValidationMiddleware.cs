d1f780e0c9849c64fbed443c7e5222fdea35afe0
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-21 20:01:44 +0300

    Middlewares

--- Source/Middlewares/CustomValidationMiddleware.cs
+++ Source/Middlewares/CustomValidationMiddleware.cs
@@ -8,75 +8,75 @@ using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace HealthHub.Source.Middlewares;
 
-public class CustomValidationMiddleware(RequestDelegate next) : ControllerBase {
+public class CustomValidationMiddleware(RequestDelegate next) : ControllerBase
+{
   Dictionary<string, string> RegisterModelErrorMessages =
-      new()
-      {
-            { "Role", "Invalid Role Provided. Role can be only Patient, Doctor or Admin" },
-            { "Gender", "Invalid Gender Provided. Gender can be only Male or Female." },
-      };
+    new()
+    {
+      { "Role", "Invalid Role Provided. Role can be only Patient, Doctor or Admin" },
+      { "Gender", "Invalid Gender Provided. Gender can be only Male or Female." },
+    };
 
-  public async Task InvokeAsync(HttpContext httpContext) {
-    try {
+  public async Task InvokeAsync(HttpContext httpContext)
+  {
+    try
+    {
       await next(httpContext); // Let other functions in the req pipeline be called
-    } catch (BadHttpRequestException ex) {
+    }
+    catch (BadHttpRequestException ex)
+    {
       httpContext.Response.ContentType = "application/problem+json";
       httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
 
       object errors = new { };
 
       // Map Errors Correctly for Model State Errors
-      if (httpContext.Items.ContainsKey(ErrorFieldConstants.ModelStateErrors)) {
-        var data = (ModelStateDictionary)
-            httpContext.Items[ErrorFieldConstants.ModelStateErrors]!;
+      if (httpContext.Items.ContainsKey(ErrorFieldConstants.ModelStateErrors))
+      {
+        var data = (ModelStateDictionary)httpContext.Items[ErrorFieldConstants.ModelStateErrors]!;
         errors = data.ToDictionary(
-            kvp => kvp.Key,
-            kvp =>
-                kvp.Value?.Errors.Select(e =>
-                        RegisterModelErrorMessages.TryGetValue(kvp.Key, out string? value)
-                            ? value
-                            : e.ErrorMessage
-                    )
-                    .ToList()
+          kvp => kvp.Key,
+          kvp =>
+            kvp.Value?.Errors.Select(e =>
+                RegisterModelErrorMessages.TryGetValue(kvp.Key, out string? value)
+                  ? value
+                  : e.ErrorMessage
+              )
+              .ToList()
         );
       }
       // Map Errors Correctly for Fluent Validation Errors
-      else if (httpContext.Items.ContainsKey(ErrorFieldConstants.FluentValidationErrors)) {
+      else if (httpContext.Items.ContainsKey(ErrorFieldConstants.FluentValidationErrors))
+      {
         errors =
-            (IDictionary<string, string[]>)
-                httpContext.Items[ErrorFieldConstants.FluentValidationErrors]!;
+          (IDictionary<string, string[]>)
+            httpContext.Items[ErrorFieldConstants.FluentValidationErrors]!;
       }
 
       await httpContext.Response.WriteAsync(
-          JsonSerializer.Serialize(
-              new ErrorResponse() { title = ex.Message, errors = errors }
-          )
+        JsonSerializer.Serialize(new ErrorResponse() { title = ex.Message, errors = errors })
       );
-    } catch (KeyNotFoundException ex) {
+    }
+    catch (KeyNotFoundException ex)
+    {
       httpContext.Response.ContentType = "application/problem+json";
       httpContext.Response.StatusCode = StatusCodes.Status404NotFound;
 
       object errors = new { };
 
       await httpContext.Response.WriteAsync(
-          JsonSerializer.Serialize(
-              new ErrorResponse() { title = ex.Message, errors = errors }
-          )
+        JsonSerializer.Serialize(new ErrorResponse() { title = ex.Message, errors = errors })
       );
-    } catch (Exception ex) {
+    }
+    catch (Exception ex)
+    {
       httpContext.Response.ContentType = "application/json";
       httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
 
-      var errorResponse = new {
-        title = "An unexpected error occurred.",
-        errors = ex.Message
-      };
+      var errorResponse = new { title = "An unexpected error occurred.", errors = ex.Message };
 
       await httpContext.Response.WriteAsync(
-          JsonSerializer.Serialize(
-              errorResponse,
-              new JsonSerializerOptions { WriteIndented = true }
-          )
+        JsonSerializer.Serialize(errorResponse, new JsonSerializerOptions { WriteIndented = true })
       );
     }
   }
