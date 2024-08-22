ed42f3d7768380efa0fc0e440b2edbf0bf296bda
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-22 17:34:10 +0300

    Made AppointmentTimeSpan Nullable For CreateAppointmentDto

--- Source/Models/Dtos/AppointmentDto.cs
+++ Source/Models/Dtos/AppointmentDto.cs
@@ -13,7 +13,7 @@ public record CreateAppointmentDto
   public required string PatientId { get; init; }
   public required string AppointmentDate { get; init; }
   public required string AppointmentTime { get; init; }
-  public required string AppointmentTimeSpan { get; init; }
+  public string? AppointmentTimeSpan { get; init; }
   public required string AppointmentType { get; init; }
 
   public void Deconstruct(
