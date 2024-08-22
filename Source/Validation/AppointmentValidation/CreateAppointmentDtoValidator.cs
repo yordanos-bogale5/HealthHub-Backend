d41cf10a7939784942f278eff7dcc7d19e155b74
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-22 03:01:47 +0300

    Feat: Validation Helper Integration

--- Source/Validation/AppointmentValidation/CreateAppointmentDtoValidator.cs
+++ Source/Validation/AppointmentValidation/CreateAppointmentDtoValidator.cs
@@ -11,57 +11,37 @@ public class CreateAppointmentDtoValidator : AbstractValidator<CreateAppointment
     RuleFor(ca => ca.DoctorId)
       .NotEmpty()
       .WithMessage("DoctorId is required.")
-      .Must(BeAValidGuid)
+      .Must(ValidationHelper.BeAValidGuid)
       .WithMessage("DoctorId must be a valid GUID.");
 
     RuleFor(ca => ca.PatientId)
       .NotEmpty()
       .WithMessage("PatientId is required.")
-      .Must(BeAValidGuid)
+      .Must(ValidationHelper.BeAValidGuid)
       .WithMessage("PatientId must be a valid GUID.");
 
     RuleFor(ca => ca.AppointmentDate)
       .NotEmpty()
       .WithMessage("AppointmentDate is required.")
-      .Must(BeAValidDateTimeString)
+      .Must(ValidationHelper.BeAValidDateTimeString)
       .WithMessage("AppointmentDate must be a valid DateTime (yyyy-MM-dd)")
-      .Must(BeNotPastDate)
+      .Must(ValidationHelper.BeNotPastDate)
       .WithMessage("AppointmentDate must not be in the past.");
 
     RuleFor(ca => ca.AppointmentTime)
       .NotEmpty()
       .WithMessage("AppointmentTime is required.")
-      .Must(BeAValidDateTimeString)
+      .Must(ValidationHelper.BeAValidDateTimeString)
       .WithMessage("AppointmentTime must be a valid DateTime (HH:mm)")
-      .Must(BeNotPastDate)
+      .Must(ValidationHelper.BeNotPastDate)
       .WithMessage("AppointmentDate must not be in the past.");
 
     RuleFor(ca => ca.AppointmentType)
       .NotEmpty()
       .WithMessage("Appointment Type is required.")
-      .Must(BeAValidAppointmentType)
+      .Must(ValidationHelper.BeAValidAppointmentType)
       .WithMessage(
         $"Appointment can only be {string.Join(", ", Enum.GetNames(typeof(AppointmentType)))}"
       );
-  }
-
-  public bool BeAValidGuid(string id)
-  {
-    return Guid.TryParse(id, out _);
-  }
-
-  public bool BeAValidDateTimeString(string date)
-  {
-    return DateTime.TryParse(date, out _);
-  }
-
-  public bool BeAValidAppointmentType(string appointmentTypeString)
-  {
-    return Enum.TryParse<AppointmentType>(appointmentTypeString, out _);
-  }
-
-  public bool BeNotPastDate(string date)
-  {
-    return DateTime.TryParse(date, out var parsedDate) && parsedDate.Date >= DateTime.UtcNow.Date;
   }
 }
