f49742da2b26b02c5f29a7ec45e2438403a7c057
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-22 17:30:37 +0300

    Migrations

--- Migrations/20240822135957_Migration_DoctorAndPatientAppointment_With_Cascade_Delete.Designer.cs
+++ Migrations/20240822135957_Migration_DoctorAndPatientAppointment_With_Cascade_Delete.Designer.cs
@@ -4,6 +4,7 @@ using HealthHub.Source.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HealthHub.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20240822135957_Migration_DoctorAndPatientAppointment_With_Cascade_Delete")]
+    partial class Migration_DoctorAndPatientAppointment_With_Cascade_Delete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
