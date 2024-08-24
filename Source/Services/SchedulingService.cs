10d7e64db0e6b40933eabbbaec77183aa8542886
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-24 20:48:38 +0300

    Feat: Scheduling Service

---
+++ Source/Services/SchedulingService.cs
@@ -0,0 +1,37 @@
+using HealthHub.Source.Data;
+using HealthHub.Source.Helpers.Extensions;
+using HealthHub.Source.Models.Enums;
+using HealthHub.Source.Models.Interfaces;
+using HealthHub.Source.Services;
+using Microsoft.EntityFrameworkCore;
+
+public class SchedulingService(ApplicationContext appContext, ILogger<SchedulingService> logger)
+{
+  public async Task<Dictionary<Days, List<TimeRange>>> GetDoctorAppointmentTimesAsync(Guid doctorId)
+  {
+    try
+    {
+      var docAppTimes = new Dictionary<Days, List<TimeRange>>();
+      var appointments = await appContext
+        .Appointments.Where(ap => ap.DoctorId == doctorId)
+        .ToListAsync();
+
+      foreach (var ap in appointments)
+      {
+        Days day = ap.AppointmentDate.DayOfWeek.ToString().ConvertToEnum<Days>();
+        if (!docAppTimes.ContainsKey(day))
+          docAppTimes[day] = new List<TimeRange>();
+
+        docAppTimes[day]
+          .Add(new TimeRange(ap.AppointmentTime, ap.AppointmentTime.Add(ap.AppointmentTimeSpan)));
+      }
+
+      return docAppTimes;
+    }
+    catch (System.Exception ex)
+    {
+      logger.LogError($"{ex}: An Error occured trying to get doctor appointment times");
+      throw;
+    }
+  }
+}
