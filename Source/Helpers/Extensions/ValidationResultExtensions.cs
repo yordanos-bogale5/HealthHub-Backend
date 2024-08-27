1fb187401937a693392bae97a229de4fd237e715
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-27 11:37:44 +0300

    Feat: Helper Method ToSnakeCase

--- Source/Helpers/Extensions/ValidationResultExtensions.cs
+++ Source/Helpers/Extensions/ValidationResultExtensions.cs
@@ -11,4 +11,30 @@ public static class ValidationResultExtensions
       .Errors.GroupBy(err => err.PropertyName)
       .ToDictionary(group => group.Key, group => group.Select(err => err.ErrorMessage).ToArray());
   }
+
+  public static string ToSnakeCase(this string str)
+  {
+    string snake_str = "";
+    foreach (char c in str)
+    {
+      if (char.IsWhiteSpace(c))
+      {
+        snake_str += '_';
+      }
+      else if (char.IsUpper(c))
+      {
+        // Only add an underscore if it's not the first character
+        if (snake_str.Length > 0)
+        {
+          snake_str += '_';
+        }
+        snake_str += char.ToLower(c);
+      }
+      else
+      {
+        snake_str += c;
+      }
+    }
+    return snake_str.Trim('_'); // Trim any leading or trailing underscores
+  }
 }
