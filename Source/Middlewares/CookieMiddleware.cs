03a567c34de9650a88009270faa7908d699b1905
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-08-27 11:38:14 +0300

    Feat: Cookie Middleware for Setting Appropriate headers for HttpOnly Cookies and etc...

---
+++ Source/Middlewares/CookieMiddleware.cs
@@ -0,0 +1,17 @@
+using System.Text.Json;
+using HealthHub.Source.Models.Dtos;
+
+public class CookieMiddleware(RequestDelegate next)
+{
+  public async Task InvokeAsync(HttpContext context)
+  {
+    var accessToken = context.Request.Cookies[AuthDefaults.AccessToken];
+
+    if (!string.IsNullOrEmpty(accessToken))
+    {
+      // Set the token in the Authorization header
+      context.Request.Headers[AuthDefaults.Authorization] = $"Bearer {accessToken}";
+    }
+    await next(context);
+  }
+}
