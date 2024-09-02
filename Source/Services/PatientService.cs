8d56c302eb081dd0a3784c3cc045f77de5261dc2
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-09-02 14:27:09 +0300

    Feat: Edit Patient Profile and Get Patient Profile Done

--- Source/Services/PatientService.cs
+++ Source/Services/PatientService.cs
@@ -36,7 +36,7 @@ public class PatientService(ILogger<PatientService> logger, ApplicationContext a
     {
       var patients = await appContext
         .Patients.Include(p => p.User)
-        .Select(p => p.ToPatientDto())
+        .Select(p => p.ToPatientDto(p.User))
         .ToListAsync();
 
       return new ServiceResponse<List<PatientDto>>
@@ -89,6 +89,69 @@ public class PatientService(ILogger<PatientService> logger, ApplicationContext a
     catch (System.Exception ex)
     {
       logger.LogError($"Failed to check if patient exists {ex}");
+      throw;
+    }
+  }
+
+  public async Task<PatientProfileDto> EditPatientProfileAsync(
+    EditPatientProfileDto editPatientProfileDto
+  )
+  {
+    try
+    {
+      var patient = await appContext
+        .Patients.Include(p => p.User)
+        .FirstOrDefaultAsync(p => p.UserId == editPatientProfileDto.UserId);
+
+      if (patient == null)
+      {
+        throw new BadHttpRequestException("Patient with that userId is not found.");
+      }
+
+      // Update fields where payload is not null
+      patient.MedicalHistory = editPatientProfileDto.MedicalHistory ?? patient.MedicalHistory;
+      patient.EmergencyContactPhone =
+        editPatientProfileDto.EmergencyContactPhone ?? patient.EmergencyContactPhone;
+      patient.EmergencyContactName =
+        editPatientProfileDto.EmergencyContactName ?? patient.EmergencyContactName;
+
+      await appContext.SaveChangesAsync();
+      return patient.ToPatientProfileDto(patient.User);
+    }
+    catch (System.Exception ex)
+    {
+      logger.LogError($"{ex}: An error occured trying to edit patient profile.");
+      throw;
+    }
+  }
+
+  /// <summary>
+  /// Retrieves the profile of the patient specified with the userId
+  /// </summary>
+  /// <param name="userId"></param>
+  /// <returns>A <see cref="PatientProfileDto"/> representing the patients's profile.</returns>
+  /// <exception cref="KeyNotFoundException"/>Thrown when no patient is found with the specified userId.<exception/>
+  public async Task<PatientProfileDto> GetPatientProfileAsync(Guid userId)
+  {
+    try
+    {
+      var patient = await appContext
+        .Patients.Where(p => p.UserId == userId)
+        .Include(p => p.User)
+        .SingleOrDefaultAsync();
+
+      if (patient == null)
+      {
+        throw new KeyNotFoundException(
+          "Patient with that user id is not found. Couldn't retrieve profile information."
+        );
+      }
+
+      return patient.ToPatientProfileDto(patient.User);
+    }
+    catch (System.Exception ex)
+    {
+      logger.LogError($"{ex}: An error occured tring to get patient profile");
       throw;
     }
   }
