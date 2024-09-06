a94df0ed2b3bd0747a5b71d13cdedf5d2345e5f5
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-09-06 16:34:17 +0300

    Registration validation for Cv, Educations & Experience

--- Source/Validation/UserValidation/RegisterUserDtoValidator.cs
+++ Source/Validation/UserValidation/RegisterUserDtoValidator.cs
@@ -1,4 +1,6 @@
 using FluentValidation;
+using HealthHub.Source.Helpers;
+using HealthHub.Source.Models.Defaults;
 using HealthHub.Source.Models.Dtos;
 using HealthHub.Source.Models.Enums;
 
@@ -55,10 +57,14 @@ public class RegisterUserDtoValidator : AbstractValidator<RegisterUserDto>
       () =>
       {
         RuleFor(u => u.Specialities)
+          .NotNull()
+          .WithMessage("Specialities cannot be null")
           .NotEmpty()
           .WithMessage("At least one speciality is required for doctors.");
 
         RuleFor(u => u.Qualifications)
+          .NotNull()
+          .WithMessage("Qualifications cannot be null")
           .NotEmpty()
           .WithMessage("Qualifications are required for doctors.");
 
@@ -67,20 +73,29 @@ public class RegisterUserDtoValidator : AbstractValidator<RegisterUserDto>
         RuleFor(u => u.DoctorStatus)
           .IsInEnum()
           .WithMessage("Doctor status must be a valid enum value.");
-      }
-    );
 
-    // Availabilities Validation
-    When(
-      u => u.Availabilities.Count > 0,
-      () =>
-      {
+        RuleFor(u => u.Cv)
+          .NotNull()
+          .WithMessage("Cv cannot be null")
+          .NotEmpty()
+          .WithMessage("Cv is required for doctors!")
+          .Must(cv => cv != null && ValidationHelper.IsImageMime(cv.MimeType))
+          .WithMessage(
+            $"The mime type provided is not valid. Available mime-types are {string.Join(",", Mime.GetImageMimes().Select(m => Mime.GetMime(m)))}"
+          )
+          .Must(cv => cv != null && ValidationHelper.IsValidBase64(cv.FileDataBase64))
+          .WithMessage("The file data is not a valid base64.");
+
         RuleFor(u => u.Availabilities)
+          .NotEmpty()
+          .WithMessage("Availability cannot be empty.")
           .Must(availabilities => availabilities.Count > 0)
           .WithMessage("At least one availability is required for doctors.");
 
         RuleForEach(u => u.Availabilities)
-          .Must(avail => Enum.TryParse<Days>(avail.Day.ToString(), true, out _))
+          .NotEmpty()
+          .WithMessage("Availability Cannot be empty")
+          .Must(avail => Enum.TryParse<DayOfWeek>(avail.Day.ToString(), true, out _))
           .WithMessage(
             "Day must be valid! (Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday)"
           )
@@ -88,6 +103,48 @@ public class RegisterUserDtoValidator : AbstractValidator<RegisterUserDto>
           .WithMessage("Start time must be valid! (HH:mm)")
           .Must(avail => TimeOnly.TryParse(avail.EndTime.ToString(), out _))
           .WithMessage("End time must be valid! (HH:mm)");
+
+        RuleFor(u => u.Educations)
+          .NotNull()
+          .NotEmpty()
+          .WithMessage("Educations cannot be empty.")
+          .Must(educations => educations.Count > 0)
+          .WithMessage("At least one education is required for doctors.");
+
+        RuleForEach(u => u.Educations)
+          .NotEmpty()
+          .WithMessage("Education is required.")
+          .Must(education => education.Degree.Trim().Length > 0)
+          .WithMessage("Education degree must be provided.")
+          .Must(education => education.Institution.Trim().Length > 0)
+          .WithMessage("Education Institution must be provided.")
+          .Must(education => ValidationHelper.BeAValidDateOnlyString(education.StartDate))
+          .WithMessage("Start date should be a valid date! (yyyy-MM-dd)")
+          .Must(education => ValidationHelper.BeAValidDateOnlyString(education.EndDate))
+          .WithMessage("End date should be a valid date! (yyyy-MM-dd)");
+
+        RuleFor(u => u.Experiences)
+          .NotNull()
+          .NotEmpty()
+          .WithMessage("Experiences cannot be empty.")
+          .Must(experiences => experiences.Count > 0)
+          .WithMessage("At least one experience is required for doctors.");
+
+        RuleForEach(u => u.Experiences)
+          .NotNull()
+          .WithMessage("Experiences cannot be null.")
+          .NotEmpty()
+          .Must(experience => experience.Institution.Trim().Length > 0)
+          .WithMessage("Please provide the institution of your experience.")
+          .Must(experience => ValidationHelper.BeAValidDateOnlyString(experience.StartDate))
+          .WithMessage("Start date should be a valid date! (yyyy-MM-dd)")
+          .Must(experience =>
+            experience.EndDate == null
+            || ValidationHelper.BeAValidDateOnlyString(experience.EndDate)
+          )
+          .WithMessage(
+            "End date should either be null or a valid date! (yyyy-MM-dd). If its null then the person is still working in the specified institute."
+          );
       }
     );
   }
