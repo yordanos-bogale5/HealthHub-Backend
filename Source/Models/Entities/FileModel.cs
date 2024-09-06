7364861a64415c012fd2120bf674922c21ce85e8
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-09-06 16:35:36 +0300

    Feat: More fields to FileModel

--- Source/Models/Entities/FileModel.cs
+++ Source/Models/Entities/FileModel.cs
@@ -1,7 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HealthHub.Source.Models.Entities;
 
-public class File {
+public class File
+{
   public Guid FileId { get; set; } = Guid.NewGuid();
+  public required string? FileName { get; set; }
   public required string MimeType { get; set; }
-  public required byte[] FileData { get; set; }
+
+  [MaxLength(5242880)] // MaxSize = 5mb file
+  public required byte[] FileData { get; set; } = [];
+  public int FileSize => FileData.Length; // Derived from FileData
+  public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
 }
