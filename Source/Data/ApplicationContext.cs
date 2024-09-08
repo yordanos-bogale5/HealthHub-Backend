7b45ee800bdfe57b7b755a675d1018871afd2fe2
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-09-08 20:46:26 +0300

    Feat: Configured SaveChanges method so that UpdatedAt field automatically reflects the modified itme

--- Source/Data/ApplicationContext.cs
+++ Source/Data/ApplicationContext.cs
@@ -1,214 +1,84 @@
+using HealthHub.Source.Config;
 using HealthHub.Source.Models.Entities;
 using HealthHub.Source.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Org.BouncyCastle.Crypto.Signers;
 
-namespace HealthHub.Source.Data
+namespace HealthHub.Source.Data;
+
+/// <summary>
+/// Represents the database context for the HealthHub application using Entity Framework Core.
+/// This class is responsible for configuring the database schema and managing entity objects.
+/// </summary>
+public class ApplicationContext : DbContext
 {
-  /// <summary>
-  /// Represents the database context for the HealthHub application using Entity Framework Core.
-  /// This class is responsible for configuring the database schema and managing entity objects.
-  /// </summary>
-  public class ApplicationContext : DbContext
+  public ApplicationContext(DbContextOptions<ApplicationContext> options)
+    : base(options) { }
+
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
   {
-    public ApplicationContext(DbContextOptions<ApplicationContext> options)
-      : base(options) { }
+    EntityConfiguration.ConfigureUniqueConstraints(modelBuilder);
+    EntityConfiguration.ConfigureForeignKeys(modelBuilder);
+    EntityConfiguration.ConfigureEnumConversions(modelBuilder);
+  }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+  public override int SaveChanges()
+  {
+    var entries = ChangeTracker
+      .Entries()
+      .Where(e => e.Entity is BaseEntity && e.State == EntityState.Modified);
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    // Each time an entity of type BaseEntity is modified we update the field UpdatedAt to reflect the correct time
+    foreach (var entry in entries)
     {
-      EntityConfiguration.ConfigureUniqueConstraints(modelBuilder);
-      EntityConfiguration.ConfigureForeignKeys(modelBuilder);
-      EntityConfiguration.ConfigureEnumConversions(modelBuilder);
+      ((BaseEntity)entry.Entity).UpdatedAt = DateTime.UtcNow;
     }
 
-    public DbSet<User> Users { get; set; }
-    public DbSet<Doctor> Doctors { get; set; }
-    public DbSet<Patient> Patients { get; set; }
-    public DbSet<Admin> Admins { get; set; }
-    public DbSet<Speciality> Specialities { get; set; }
-    public DbSet<DoctorSpeciality> DoctorSpecialities { get; set; }
-
-    public DbSet<Education> Educations { get; set; }
-    public DbSet<Experience> Experiences { get; set; }
-
-    public DbSet<Appointment> Appointments { get; set; }
-    public DbSet<DoctorAvailability> DoctorAvailabilities { get; set; }
-    public DbSet<Blog> Blogs { get; set; }
-    public DbSet<BlogComment> BlogComments { get; set; }
-    public DbSet<BlogLike> BlogLikes { get; set; }
-    public DbSet<Review> Reviews { get; set; }
-    public DbSet<Chat> Chats { get; set; }
-    public DbSet<Message> Messages { get; set; }
-    public DbSet<Notification> Notifications { get; set; }
-    public DbSet<Models.Entities.File> Files { get; set; }
+    return base.SaveChanges();
   }
-}
 
-internal static class EntityConfiguration
-{
-  internal static void ConfigureUniqueConstraints(ModelBuilder mb)
+  public override Task<int> SaveChangesAsync(
+    bool acceptAllChangesOnSuccess,
+    CancellationToken cancellationToken = default
+  )
   {
-    // User Entity Configurations
-    var user = mb.Entity<User>();
+    var entries = ChangeTracker
+      .Entries()
+      .Where(e => e.Entity is BaseEntity && e.State == EntityState.Modified);
 
-    user.HasIndex(u => u.Email).IsUnique();
-    user.HasIndex(u => u.Phone).IsUnique();
+    // Each time an entity of type BaseEntity is modified we update the field UpdatedAt to reflect the correct time
+    foreach (var entry in entries)
+    {
+      ((BaseEntity)entry.Entity).UpdatedAt = DateTime.UtcNow;
+    }
 
-    var speciality = mb.Entity<Speciality>();
-    speciality.HasIndex(s => s.SpecialityName).IsUnique();
+    return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
   }
 
-  internal static void ConfigureForeignKeys(ModelBuilder mb)
-  {
-    var appointment = mb.Entity<Appointment>();
+  public DbSet<User> Users { get; set; }
+  public DbSet<Doctor> Doctors { get; set; }
+  public DbSet<Patient> Patients { get; set; }
+  public DbSet<Admin> Admins { get; set; }
+  public DbSet<Speciality> Specialities { get; set; }
+  public DbSet<DoctorSpeciality> DoctorSpecialities { get; set; }
 
-    // Restrict the deletion of the Appointment table if the Doctor entity is deleted
-    appointment
-      .HasOne(a => a.Doctor)
-      .WithMany()
-      .HasForeignKey(a => a.DoctorId)
-      .OnDelete(DeleteBehavior.Restrict);
+  public DbSet<Education> Educations { get; set; }
+  public DbSet<Experience> Experiences { get; set; }
 
-    // Restrict the deletion of the Appointment table if the Patient entity is deleted
-    appointment
-      .HasOne(a => a.Patient)
-      .WithMany()
-      .HasForeignKey(a => a.PatientId)
-      .OnDelete(DeleteBehavior.Restrict);
+  public DbSet<Appointment> Appointments { get; set; }
+  public DbSet<DoctorAvailability> DoctorAvailabilities { get; set; }
+  public DbSet<Blog> Blogs { get; set; }
+  public DbSet<BlogComment> BlogComments { get; set; }
+  public DbSet<BlogLike> BlogLikes { get; set; }
+  public DbSet<Review> Reviews { get; set; }
 
-    var chat = mb.Entity<Chat>();
+  public DbSet<Conversation> Conversations { get; set; }
+  public DbSet<ConversationMembership> ConversationMemberships { get; set; }
+  public DbSet<Message> Messages { get; set; }
 
-    // Restrict the deletion of the Chat table if the Doctor entity is deleted
-    chat.HasOne(c => c.Sender)
-      .WithMany()
-      .HasForeignKey(c => c.SenderId)
-      .OnDelete(DeleteBehavior.Restrict);
-
-    // Restrict the deletion of the Chat table if the Patient entity is deleted
-    chat.HasOne(c => c.Receiver)
-      .WithMany()
-      .HasForeignKey(c => c.ReceiverId)
-      .OnDelete(DeleteBehavior.Restrict);
-
-    var payment = mb.Entity<Payment>();
-
-    // Restrict the deletion of the Payment table if the Doctor entity is deleted
-    payment
-      .HasOne(p => p.Doctor)
-      .WithMany()
-      .HasForeignKey(p => p.DoctorId)
-      .OnDelete(DeleteBehavior.Restrict);
-
-    // Restrict the deletion of the Payment table if the Patient entity is deleted
-    payment
-      .HasOne(p => p.Patient)
-      .WithMany()
-      .HasForeignKey(p => p.PatientId)
-      .OnDelete(DeleteBehavior.Restrict);
-
-    mb.Entity<DoctorSpeciality>().HasKey(ds => new { ds.DoctorId, ds.SpecialityId }); // Comp Key
-
-    mb.Entity<DoctorSpeciality>()
-      .HasOne(ds => ds.Doctor)
-      .WithMany(d => d.DoctorSpecialities)
-      .HasForeignKey(ds => ds.DoctorId)
-      .OnDelete(DeleteBehavior.Cascade);
-
-    mb.Entity<DoctorSpeciality>()
-      .HasOne(ds => ds.Speciality)
-      .WithMany(s => s.DoctorSpecialities)
-      .HasForeignKey(ds => ds.SpecialityId)
-      .OnDelete(DeleteBehavior.Cascade);
-
-    // This can be optional if we want to preserve the blog even if the user gets deleted
-    // or Cascade deletion of blog when the user who wrote that blog gets deleted
-    mb.Entity<Blog>()
-      .HasOne(b => b.Author)
-      .WithMany(a => a.Blogs)
-      .HasForeignKey(b => b.AuthorId)
-      .OnDelete(DeleteBehavior.NoAction);
-
-    // Same here too
-    // Cascade delete Blog Comment when Blog or User who wrote that comment gets deleted
-    mb.Entity<BlogComment>()
-      .HasOne(bc => bc.Blog)
-      .WithMany(b => b.BlogComments)
-      .HasForeignKey(bc => bc.BlogId)
-      .OnDelete(DeleteBehavior.Cascade); // No need to keep a comment if a blog gets deleted
-
-    mb.Entity<BlogComment>()
-      .HasOne(bc => bc.Sender)
-      .WithMany(s => s.BlogComments)
-      .HasForeignKey(bc => bc.SenderId) // Corrected Foreign Key
-      .OnDelete(DeleteBehavior.Restrict); // Restrict delete to keep comments from deleted users
-
-    // Doctor Availability
-    mb.Entity<DoctorAvailability>()
-      .HasOne(da => da.Doctor)
-      .WithMany(d => d.DoctorAvailabilities)
-      .HasForeignKey(da => da.DoctorId)
-      .OnDelete(DeleteBehavior.Cascade);
-
-    // Doctor Appointments
-    mb.Entity<Appointment>()
-      .HasOne(a => a.Doctor)
-      .WithMany(d => d.Appointments)
-      .HasForeignKey(a => a.DoctorId)
-      .OnDelete(DeleteBehavior.Restrict);
-
-    // Patient Appointments
-    mb.Entity<Appointment>()
-      .HasOne(a => a.Patient)
-      .WithMany(p => p.Appointments)
-      .HasForeignKey(a => a.PatientId)
-      .OnDelete(DeleteBehavior.Cascade);
-  }
-
-  internal static void ConfigureEnumConversions(ModelBuilder mb)
-  {
-    // For User
-    mb.Entity<User>().Property(u => u.Role).HasConversion<string>();
-    mb.Entity<User>().Property(u => u.Gender).HasConversion<string>();
-
-    // For Doctor
-    mb.Entity<Doctor>().Property(d => d.DoctorStatus).HasConversion<string>();
-
-    // For Appointment
-    mb.Entity<Appointment>().Property(a => a.AppointmentType).HasConversion<string>();
-    mb.Entity<Appointment>().Property(a => a.Status).HasConversion<string>();
-
-    // For Paymentt
-    mb.Entity<Payment>().Property(r => r.PaymentStatus).HasConversion<string>();
-    mb.Entity<Payment>().Property(r => r.PaymentProvider).HasConversion<string>();
-
-    // For Notification
-    mb.Entity<Notification>().Property(r => r.NotificationType).HasConversion<string>();
-
-    // For DoctorAvailability
-    mb.Entity<DoctorAvailability>().Property(da => da.AvailableDay).HasConversion<string>();
-
-    // Configure Doctor education relationship
-    mb.Entity<Doctor>()
-      .HasMany(d => d.Educations)
-      .WithOne(e => e.Doctor)
-      .HasForeignKey(e => e.DoctorId)
-      .OnDelete(DeleteBehavior.Cascade);
-
-    // Configure doctor experience relationship
-    mb.Entity<Doctor>()
-      .HasMany(d => d.Experiences)
-      .WithOne(e => e.Doctor)
-      .HasForeignKey(e => e.DoctorId)
-      .OnDelete(DeleteBehavior.Cascade);
-
-    // Configure Cv relationship with doctor
-    mb.Entity<Doctor>()
-      .HasOne(d => d.Cv)
-      .WithOne()
-      .HasForeignKey<Doctor>(d => d.CvId)
-      .OnDelete(DeleteBehavior.Cascade);
-  }
+  public DbSet<Notification> Notifications { get; set; }
+  public DbSet<Models.Entities.File> Files { get; set; }
 }
