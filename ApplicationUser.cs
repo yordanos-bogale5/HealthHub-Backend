29c84959e162516598e26a2795bc1acb2029d84e
Abrham <133606313+Abrham21-lab@users.noreply.github.com>
2024-09-14 12:15:12 +0300

    Add files via upload

---
+++ ApplicationUser.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Chat.Data
+{
+    public class ApplicationUser { }
+}
