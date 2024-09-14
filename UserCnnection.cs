35c372e7171bc5d8e6f9f9fab0a7eaf91a23592d
Abrham <133606313+Abrham21-lab@users.noreply.github.com>
2024-09-14 12:48:05 +0300

    Add files via upload

---
+++ UserCnnection.cs
@@ -0,0 +1,14 @@
+using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat.Models
+{
+    public class UserCnnection { 
+
+        public string username{get;set;}=String.Empty;
+         public string ChatRoom{get;set;}=String.Empty;
+    }
+}
