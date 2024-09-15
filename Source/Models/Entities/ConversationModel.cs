09941e9b6a13f21eaea0c833e7ae56e3c35e6f99
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-09-15 13:48:46 +0300

    Changed Chat Model to Conversation model instead and removed unnecessary SenderId and ReceiverId from it.

---
+++ Source/Models/Entities/ConversationModel.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HealthHub.Source.Models.Entities;
+
+public class Conversation : BaseEntity
+{
+  public Guid ConversationId { get; set; } = Guid.NewGuid(); // <<PK>>
+  public virtual ICollection<Message> Messages { get; set; } = new HashSet<Message>();
+}
