6094721da4620b2289f8f924ebebe6ac4c5a3f6f
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-09-17 20:08:08 +0300

    Fix: Attributes

--- Source/Attributes/GuidAttribute.cs
+++ Source/Attributes/GuidAttribute.cs
@@ -5,9 +5,11 @@ public class GuidAttribute : ValidationAttribute
   protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
   {
     if (value == null)
+    {
       return new ValidationResult("Guid cannot be null");
+    }
 
-    if (!Guid.TryParse(value.ToString(), out Guid parsed))
+    if (!Guid.TryParse(value.ToString(), out Guid parsed) || parsed == Guid.Empty)
     {
       return new ValidationResult("Value is not a valid guid string.");
     }
--- Source/Attributes/PaymentProviderAttribute.cs
+++ Source/Attributes/PaymentProviderAttribute.cs
@@ -1,23 +1,41 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 using HealthHub.Source.Models.Enums;
 
-public class PaymentProviderAttribute(bool required = true) : ValidationAttribute
+public class PaymentProviderAttribute : ValidationAttribute
 {
+  private readonly bool _required;
+
+  public PaymentProviderAttribute(bool required = true)
+  {
+    _required = required;
+  }
+
   protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
   {
-    if (value is null && required)
-      return new ValidationResult("Payment provider is required.");
-
-    var providers = Enum.GetNames<PaymentProvider>();
-
-    if (
-      value is not PaymentProvider paymentProvider
-      || !Enum.IsDefined(typeof(PaymentProvider), paymentProvider)
-    )
+    // If the value is null and the field is required, return an error
+    if (value is null && _required)
     {
-      return new ValidationResult(
-        $"Invalid payment provider. Payment providers can only be {string.Join(",", providers)}"
-      );
+      return new ValidationResult("Payment provider is required.");
+    }
+
+    // Check if the value is a valid PaymentProvider enum
+    if (value is PaymentProvider paymentProvider)
+    {
+      if (
+        !Enum.IsDefined(typeof(PaymentProvider), paymentProvider)
+        || paymentProvider == PaymentProvider.Unknown
+      )
+      {
+        var providers = Enum.GetNames<PaymentProvider>();
+        return new ValidationResult(
+          $"Invalid payment provider. Payment providers can only be {string.Join(", ", providers.Skip(1))}."
+        );
+      }
+    }
+    else
+    {
+      return new ValidationResult("Invalid payment provider value.");
     }
     return ValidationResult.Success;
   }
