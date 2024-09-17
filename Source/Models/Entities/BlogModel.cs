47298919222be41555efa7718fc9e42bdf064405
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-09-17 21:28:58 +0300

    Feat: Added Slug, Summary and Collection of Tags for Blog Model

--- Source/Models/Entities/BlogModel.cs
+++ Source/Models/Entities/BlogModel.cs
@@ -6,18 +6,21 @@ public class Blog : BaseEntity
 {
   public Guid BlogId { get; set; }
 
-  [Required]
   public Guid AuthorId { get; set; } // <<FK>>
 
-  [Required]
   public required string Title { get; set; }
 
-  [Required]
   public required string Content { get; set; }
+
+  public required string Slug { get; set; }
+
+  public required string Summary { get; set; }
 
   public virtual required User Author { get; set; }
 
   public virtual ICollection<BlogComment> BlogComments { get; set; } = new HashSet<BlogComment>();
 
   public virtual ICollection<BlogLike> BlogLikes { get; set; } = new HashSet<BlogLike>();
+
+  public virtual ICollection<Tag> Tags { get; set; } = new HashSet<Tag>();
 }
