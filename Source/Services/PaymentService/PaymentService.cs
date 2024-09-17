f0efdd6541c7e30e6650153c3e3423b839f81307
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-09-17 20:20:34 +0300

    Feat: 🚀 Payment Service - integration with paymentProvider and database

--- Source/Services/PaymentService/PaymentService.cs
+++ Source/Services/PaymentService/PaymentService.cs
@@ -1,3 +1,6 @@
+using HealthHub.Source.Data;
+using HealthHub.Source.Helpers.Extensions;
+using HealthHub.Source.Models.Entities;
 using HealthHub.Source.Models.Enums;
 using HealthHub.Source.Services.PaymentProviders;
 
@@ -5,6 +8,9 @@ namespace HealthHub.Source.Services.PaymentService;
 
 public class PaymentService(
   IPaymentProviderFactory paymentProviderFactory,
+  DoctorService doctorService,
+  PatientService patientService,
+  ApplicationContext appContext,
   ILogger<PaymentService> logger
 ) : IPaymentService
 {
@@ -23,22 +29,66 @@ public class PaymentService(
     }
   }
 
-  public async Task<bool> TransferAsync(
-    string senderEmail,
-    string receiverEmail,
-    decimal amount,
-    PaymentProvider provider
+  public async Task<TransferResponseDto> TransferAsync(
+    TransferRequestDto transferRequestDto,
+    Guid senderId
   )
   {
     try
     {
-      IPaymentProvider paymentProvider = paymentProviderFactory.GetProvider(provider);
-      var result = await paymentProvider.TransferAsync(senderEmail, receiverEmail, amount);
-      return result;
+      IPaymentProvider paymentProvider = paymentProviderFactory.GetProvider(
+        transferRequestDto.PaymentProvider
+      );
+
+      // Perform the transfer
+      var result = await paymentProvider.TransferAsync(transferRequestDto);
+
+      // Create a payment record
+      var payment = await CreatePaymentAsync(
+        transferRequestDto.ToCreatePaymentDto(senderId, result.IsSuccessful),
+        result.TransactionReference
+      );
+
+      // TODO
+
+      return new TransferResponseDto
+      {
+        IsSuccessful = result.IsSuccessful,
+        Message = new { result.Message, payment }
+      };
     }
     catch (System.Exception ex)
     {
       logger.LogError(ex, "Error transferring funds");
+      throw;
+    }
+  }
+
+  public async Task<PaymentDto> CreatePaymentAsync(
+    CreatePaymentDto createPaymentDto,
+    string transactionReference
+  )
+  {
+    try
+    {
+      if (!await doctorService.UserExistsAsync(createPaymentDto.ReceiverId))
+      {
+        throw new ArgumentException("Doctor with the specified receiver_id does not exist");
+      }
+
+      if (!await patientService.UserExistsAsync(createPaymentDto.SenderId))
+      {
+        throw new ArgumentException("Patient with the specified user_id does not exist");
+      }
+
+      var payment = createPaymentDto.ToPayment(transactionReference);
+      var result = await appContext.Payments.AddAsync(payment);
+      await appContext.SaveChangesAsync();
+      return result.Entity.ToPaymentDto();
+    }
+    catch (System.Exception ex)
+    {
+      logger.LogError(ex, "Error creating payment");
       throw;
     }
   }
