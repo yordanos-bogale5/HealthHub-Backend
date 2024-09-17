de127ef130a64770da16a808b888d3b98af7bcd4
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-09-18 01:01:13 +0300

    Feat: Create Blog ApiRoute

--- Source/Controllers/BlogController.cs
+++ Source/Controllers/BlogController.cs
@@ -1,9 +1,11 @@
+using HealthHub.Source.Helpers.Defaults;
 using HealthHub.Source.Models.Dtos;
+using HealthHub.Source.Services.BlogService;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
 [Route("api/blogs")]
-public class BlogController() : ControllerBase
+public class BlogController(IBlogService blogService) : ControllerBase
 {
   [HttpGet("all")]
   public IActionResult GetAllBlogs() => throw new NotImplementedException();
@@ -12,8 +14,23 @@ public class BlogController() : ControllerBase
   public IActionResult GetBlogById([FromRoute] Guid id) => throw new NotImplementedException();
 
   [HttpPost]
-  public IActionResult CreateBlog([FromBody] CreateBlogDto createBlogDto) =>
-    throw new NotImplementedException();
+  public async Task<IActionResult> CreateBlog([FromBody] CreateBlogDto createBlogDto)
+  {
+    try
+    {
+      if (!ModelState.IsValid)
+      {
+        HttpContext.Items[ErrorFieldConstants.ModelStateErrors] = ModelState;
+        throw new BadHttpRequestException(ErrorMessages.ModelValidationError);
+      }
+      var result = await blogService.CreateBlogAsync(createBlogDto);
+      return Ok(result);
+    }
+    catch (System.Exception ex)
+    {
+      throw;
+    }
+  }
 
   [HttpPut("{blogId}")]
   public IActionResult UpdateBlog(
