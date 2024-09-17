c6cd7f202e8775f3662ef210f5917c7524813071
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-09-17 20:12:43 +0300

    Feat: 🚀 Transfer Balance Api Controller with appropriate validation

--- Source/Controllers/PaymentController.cs
+++ Source/Controllers/PaymentController.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using HealthHub.Source.Helpers.Defaults;
 using HealthHub.Source.Models.Enums;
 using HealthHub.Source.Services.PaymentService;
@@ -18,6 +19,41 @@ public class PaymentController(IPaymentService paymentService, ILogger<PaymentCo
     {
       var balance = await paymentService.CheckBalanceAsync(email, paymentProvider);
       return Ok(balance);
+    }
+    catch (System.Exception ex)
+    {
+      logger.LogError(ex, "Error checking balance");
+      throw;
+    }
+  }
+
+  [HttpPost("transfer")]
+  public async Task<IActionResult> TransferBalance([Required] TransferRequestDto transferRequestDto)
+  {
+    try
+    {
+      if (!ModelState.IsValid)
+      {
+        HttpContext.Items[ErrorFieldConstants.ModelStateErrors] = ModelState;
+        throw new BadHttpRequestException(ErrorMessages.ModelValidationError);
+      }
+
+      var userId = Request.Cookies[CookieDefaults.Profile.UserId]?.ToString();
+
+      if (string.IsNullOrEmpty(userId))
+      {
+        throw new ArgumentException(
+          "Either you haven't logged in or the cookie for userId is missing."
+        );
+      }
+
+      if (!Guid.TryParse(userId, out var senderId))
+      {
+        throw new FormatException("Invalid userId format. Please login again.");
+      }
+
+      var result = await paymentService.TransferAsync(transferRequestDto, senderId);
+      return Ok(result);
     }
     catch (System.Exception ex)
     {
