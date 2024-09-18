8f05532064b2c4c6d345d2338484e7f0be9eff55
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-09-18 10:20:58 +0300

    Feat: GetAllBlogs, GetBlogById, CreateBlog

--- Source/Controllers/BlogController.cs
+++ Source/Controllers/BlogController.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using HealthHub.Source.Helpers.Defaults;
 using HealthHub.Source.Models.Dtos;
 using HealthHub.Source.Services.BlogService;
@@ -5,24 +6,44 @@ using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
 [Route("api/blogs")]
-public class BlogController(IBlogService blogService) : ControllerBase
+public class BlogController(IBlogService blogService, ILogger<BlogController> logger)
+  : ControllerBase
 {
   [HttpGet("all")]
-  public IActionResult GetAllBlogs() => throw new NotImplementedException();
+  public async Task<IActionResult> GetAllBlogs()
+  {
+    try
+    {
+      var result = await blogService.GetAllBlogsAsync();
+      return Ok(result);
+    }
+    catch (System.Exception ex)
+    {
+      logger.LogError(ex, "An error occured trying to get all the blogs.");
+      throw;
+    }
+  }
 
-  [HttpGet("{id}")]
-  public IActionResult GetBlogById([FromRoute] Guid id) => throw new NotImplementedException();
+  [HttpGet("{blogId}")]
+  public async Task<IActionResult> GetBlogById([FromRoute] [Required] [Guid] Guid blogId)
+  {
+    try
+    {
+      var result = await blogService.GetBlogAsync(blogId);
+      return Ok(result);
+    }
+    catch (System.Exception ex)
+    {
+      logger.LogError(ex, "An error occured trying to get blog by id.");
+      throw;
+    }
+  }
 
   [HttpPost]
   public async Task<IActionResult> CreateBlog([FromBody] CreateBlogDto createBlogDto)
   {
     try
     {
-      if (!ModelState.IsValid)
-      {
-        HttpContext.Items[ErrorFieldConstants.ModelStateErrors] = ModelState;
-        throw new BadHttpRequestException(ErrorMessages.ModelValidationError);
-      }
       var result = await blogService.CreateBlogAsync(createBlogDto);
       return Ok(result);
     }
