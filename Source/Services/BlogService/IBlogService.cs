cc2d1a713512db0c71e871d29aadd2c4ce15ea71
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-09-18 14:54:38 +0300

    IBlogService: More CRUD Methods

--- Source/Services/BlogService/IBlogService.cs
+++ Source/Services/BlogService/IBlogService.cs
@@ -1,4 +1,5 @@
 using HealthHub.Source.Models.Dtos;
+using HealthHub.Source.Models.Entities;
 
 namespace HealthHub.Source.Services.BlogService;
 
@@ -7,6 +8,10 @@ public interface IBlogService
   Task<BlogDto> CreateBlogAsync(CreateBlogDto createBlogDto);
   Task<List<BlogDto>> GetAllBlogsAsync();
   Task<BlogDto> GetBlogAsync(Guid blogId);
-  Task<BlogDto> UpdateBlogAsync(Guid blogId, CreateBlogDto createBlogDto);
+  Task<BlogDto> UpdateBlogAsync(Guid blogId, EditBlogDto editBlogDto);
   Task<bool> SlugExistsAsync(string slug);
+  Task<bool> BlogExistsAsync(Guid blogId);
+  void DeleteAllBlogs();
+  Task<ICollection<Tag>> CreateTagsAsync(IList<string> tags);
+  Task<ICollection<BlogTag>> CreateBlogTagAssocAsync(Guid blogId, ICollection<Tag> tags);
 }
