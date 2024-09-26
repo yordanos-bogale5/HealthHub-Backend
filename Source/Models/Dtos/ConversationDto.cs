eaa7ccb9d6cfff4a178f3eae39f15509d95a336c
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-09-26 22:47:14 +0300

    Create Conversation Dto

--- Source/Models/Dtos/ConversationDto.cs
+++ Source/Models/Dtos/ConversationDto.cs
@@ -1,4 +1,7 @@
+using System.ComponentModel.DataAnnotations;
 using HealthHub.Source.Models.Dtos;
+using Microsoft.AspNetCore.Antiforgery;
+using Xunit;
 
 public interface IConversationDto
 {
@@ -10,4 +13,11 @@ public class ConversationDtoBase : IConversationDto
 {
   public required Guid ConversationId { get; set; }
   public required ICollection<IProfileDto> Participants { get; set; }
+}
+
+public class CreateConversationDto
+{
+  [Required]
+  [GuidCollection]
+  public required ICollection<Guid> Participants = [];
 }
