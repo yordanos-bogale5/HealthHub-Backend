518362ebe580df47edb5dbc372b294cd0a9d06cf
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-09-26 22:43:19 +0300

    Feat: Setup Endpoints like ChargeCustomer, ChapaCallback, and ChapaPaymentWebhook

--- Source/Controllers/PaymentController.cs
+++ Source/Controllers/PaymentController.cs
@@ -1,31 +1,47 @@
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using HealthHub.Source.Config;
 using HealthHub.Source.Helpers.Defaults;
 using HealthHub.Source.Models.Enums;
+using HealthHub.Source.Models.Interfaces.ChapaProvider;
+using HealthHub.Source.Models.Interfaces.Payments;
+using HealthHub.Source.Models.Responses;
 using HealthHub.Source.Services.PaymentService;
+using MailKit;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 [ApiController]
 [Route("api/payments")]
-public class PaymentController(IPaymentService paymentService, ILogger<PaymentController> logger)
-  : ControllerBase
+public class PaymentController(
+  IPaymentService paymentService,
+  ILogger<PaymentController> logger,
+  AppConfig appConfig
+) : ControllerBase
 {
-  [HttpGet("balance/{email}/{paymentProvider}")]
-  public async Task<IActionResult> CheckBalance(
-    [FromRoute] string email,
-    [FromRoute] [PaymentProvider] PaymentProvider paymentProvider
-  )
-  {
-    try
-    {
-      var balance = await paymentService.CheckBalanceAsync(email, paymentProvider);
-      return Ok(balance);
-    }
-    catch (System.Exception ex)
-    {
-      logger.LogError(ex, "Error checking balance");
-      throw;
-    }
-  }
+  private string requestBody;
+
+  // [HttpGet("balance/{email}/{paymentProvider}")]
+  // public async Task<IActionResult> CheckBalance(
+  //   [FromRoute] string email,
+  //   [FromRoute] [PaymentProvider] PaymentProvider paymentProvider
+  // )
+  // {
+  //   try
+  //   {
+  //     var balance = await paymentService.CheckBalanceAsync(email, paymentProvider);
+  //     return Ok(balance);
+  //   }
+  //   catch (System.Exception ex)
+  //   {
+  //     logger.LogError(ex, "Error checking balance");
+  //     throw;
+  //   }
+  // }
 
   [HttpPost("transfer")]
   public async Task<IActionResult> TransferBalance([Required] TransferRequestDto transferRequestDto)
@@ -58,6 +74,77 @@ public class PaymentController(IPaymentService paymentService, ILogger<PaymentCo
     catch (System.Exception ex)
     {
       logger.LogError(ex, "Error checking balance");
+      throw;
+    }
+  }
+
+  [HttpPost("charge")]
+  public async Task<IActionResult> ChargeCustomer([Required] ChargeRequest chargeRequest)
+  {
+    try
+    {
+      var result = await paymentService.ChargeAsync(chargeRequest);
+      return Ok(new ApiResponse<ChargeResponse>(result.Status, result.Message, result));
+    }
+    catch (System.Exception ex)
+    {
+      logger.LogError(ex, "Error charging customer");
+      throw;
+    }
+  }
+
+  [HttpPost("chapa/callback")]
+  public async Task<IActionResult> ChapaCallback([FromBody] string status, [FromBody] string tx_rf)
+  {
+    try
+    {
+      // var result = await paymentService.VerifyAsync(
+      //   new VerifyRequest { TransactionReference = tx_rf }
+      // );
+
+      var result = await Task.FromResult("");
+
+      return Ok(new { status, tx_rf });
+    }
+    catch (System.Exception ex)
+    {
+      logger.LogError(ex, "Error while processing chapa callback");
+      throw;
+    }
+  }
+
+  /// <summary>
+  /// For intergration with Chapa payment provider, please refer to the documentation
+  /// ref: https://developer.chapa.co/integrations/webhooks
+  /// </summary>
+  /// <param name="body"></param>
+  /// <returns></returns>
+  [HttpPost("chapa/webhooks")]
+  public async Task<IActionResult> ChapaPaymentWebhooks([FromBody] object body)
+  {
+    try
+    {
+      var chapaSignature = Request.Headers["Chapa-Signature"].ToString();
+      var requestBody = JsonConvert.SerializeObject(body);
+
+      var hash = EncryptionHelper.GetHmacSha256Hash(
+        requestBody,
+        appConfig.WebhookSecret
+          ?? throw new Exception("No secret key. Please check the configuration.")
+      );
+
+      if (hash != chapaSignature)
+      {
+        throw new ArgumentException(
+          $"Invalid signature.\nHash: {hash}\nSignature: {chapaSignature}"
+        );
+      }
+
+      return Ok();
+    }
+    catch (System.Exception ex)
+    {
+      logger.LogError(ex, "Error while checking payment webhooks.");
       throw;
     }
   }
