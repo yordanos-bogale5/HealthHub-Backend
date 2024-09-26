71a2b8a1ccd6027e20232b2e3b47d51a44b9c7e1
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-09-26 22:49:14 +0300

    Review Data transfer objecs

---
+++ Source/Models/Dtos/ReviewDto.cs
@@ -0,0 +1,42 @@
+using System.ComponentModel.DataAnnotations;
+using HealthHub.Source.Attributes;
+
+public record ReviewDto
+{
+  public required Guid ReviewId { get; set; }
+  public required Guid DoctorId { get; set; }
+  public required Guid? PatientId { get; set; }
+  public required decimal StarRating { get; set; }
+  public required string ReviewText { get; set; }
+};
+
+public record CreateReviewDto
+{
+  [Required]
+  public required Guid DoctorId { get; set; }
+
+  [Required]
+  public required Guid? PatientId { get; set; }
+
+  [Required]
+  [StarRating]
+  [Range(0, 5)]
+  public required decimal StarRating { get; set; }
+
+  [Required]
+  public required string ReviewText { get; set; }
+};
+
+public record EditReviewDto
+{
+  [Required]
+  public required Guid ReviewId { get; set; }
+
+  [Required]
+  [StarRating]
+  [Range(0, 5)]
+  public required decimal StarRating { get; set; }
+
+  [Required]
+  public required string ReviewText { get; set; }
+};
