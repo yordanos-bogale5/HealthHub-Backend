c323ecb8b844a2d76575ab6cb24ccadfdff8c614
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-09-26 22:48:33 +0300

    Minor changes to reflect the new model structure

--- Source/Models/Dtos/MessageDto.cs
+++ Source/Models/Dtos/MessageDto.cs
@@ -1,17 +1,11 @@
 using System.ComponentModel.DataAnnotations;
 using HealthHub.Source.Views;
 
-public record MessageDto(
-  Guid MessageId,
-  Guid SenderId,
-  Guid ReceiverId,
-  string? MessageText,
-  List<FileDto>? Files
-);
+public record MessageDto(Guid MessageId, Guid? SenderId, string? MessageText, List<FileDto>? Files);
 
 public record CreateMessageDto(
+  [Guid] Guid ConversationId,
   [Guid] Guid SenderId,
-  [Guid] Guid ReceiverId,
   [MinLength(1)] string? MessageText,
   [ValidCreateFileList] List<CreateFileDto>? Files
 );
