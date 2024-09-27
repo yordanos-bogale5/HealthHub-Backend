b3b269ebc5b9a60e595d50133a8831740ba02d96
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-09-27 13:30:15 +0300

    Constraints

--- Source/Models/Entities/ReviewModel..cs
+++ Source/Models/Entities/ReviewModel..cs
@@ -8,14 +8,19 @@ public class Review : BaseEntity
   public Guid ReviewId { get; set; }
 
   [Required]
-  public Guid PaymentId { get; set; } // <<FK>>
+  public Guid DoctorId { get; set; } // <<FK>>
+
+  [Required]
+  public Guid PatientId { get; set; } // <<FK>>
 
   [Required]
   [StarRating]
+  [Range(0, 5)]
   public decimal StarRating { get; set; }
 
   [Required]
   public required string ReviewText { get; set; }
 
-  public virtual required Payment Payment { get; set; }
+  public virtual Doctor? Doctor { get; set; }
+  public virtual Patient? Patient { get; set; }
 }
