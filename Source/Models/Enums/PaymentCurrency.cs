ebcead3f7585afd14ac45ba029eb5aad3e8d1f11
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-09-27 13:30:54 +0300

    Payment Currency and Payment Types

---
+++ Source/Models/Enums/PaymentCurrency.cs
@@ -0,0 +1,8 @@
+namespace HealthHub.Source.Models.Enums;
+
+public enum PaymentCurrency
+{
+  Unknown = 0,
+  ETB,
+  // Add more currencies here
+}
---
+++ Source/Models/Enums/PaymentType.cs
@@ -0,0 +1,8 @@
+namespace HealthHub.Source.Models.Enums;
+
+public enum PaymentType
+{
+  Unknown = 0,
+  Transfer,
+  Charge
+}
