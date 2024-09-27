e209d9d64ab5b2ff26d0a87107c853ac1cba1f36
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-09-27 13:32:16 +0300

    Chapa Payment Provider ChargeAsync Method IMpl

--- Source/Services/PaymentProviders/ChapaPaymentProvider.cs
+++ Source/Services/PaymentProviders/ChapaPaymentProvider.cs
@@ -1,10 +1,15 @@
 using System.Text.Json;
+using System.Text.Json.Nodes;
 using ChapaNET;
 using HealthHub.Source.Config;
+using HealthHub.Source.Helpers.Extensions;
 using HealthHub.Source.Models.Enums;
+using HealthHub.Source.Models.Interfaces.Payments;
+using HealthHub.Source.Models.Interfaces.Payments.Chapa;
 using HealthHub.Source.Services.PaymentProviders;
 using Newtonsoft.Json.Linq;
 using RestSharp;
+using Xunit.Sdk;
 
 public class ChapaPaymentProvider(AppConfig appConfig, ILogger<ChapaPaymentProvider> logger)
   : IPaymentProvider
@@ -25,9 +30,7 @@ public class ChapaPaymentProvider(AppConfig appConfig, ILogger<ChapaPaymentProvi
         throw new Exception("Chapa Secret Key is not set");
       }
 
-      Chapa chapa = new(appConfig.ChapaSecretKey);
-
-      var tx_rf = Chapa.GetUniqueRef();
+      var tx_rf = PaymentHelper.GetTransactionReference();
 
       var restClient = new RestClient();
       var request = new RestRequest(
@@ -42,8 +45,7 @@ public class ChapaPaymentProvider(AppConfig appConfig, ILogger<ChapaPaymentProvi
         {
           email = transferRequestDto.SenderEmail,
           amount = transferRequestDto.Amount,
-          first_name = transferRequestDto.FirstName,
-          last_name = transferRequestDto.LastName,
+          first_name = transferRequestDto.SenderName,
           tx_ref = tx_rf,
           currency = "ETB",
           callback_url = appConfig.ApiOrigin,
@@ -79,6 +81,119 @@ public class ChapaPaymentProvider(AppConfig appConfig, ILogger<ChapaPaymentProvi
     catch (System.Exception ex)
     {
       logger.LogError(ex, "Error transferring funds");
+      throw;
+    }
+  }
+
+  public async Task<IChargeResponse> ChargeAsync(ICharge charge)
+  {
+    try
+    {
+      if (charge is not ChapaCharge chapaCharge)
+        throw new InvalidOperationException("Charge type must be of type ChapaCharge");
+
+      var restClient = new RestClient();
+      restClient.AddDefaultHeader("Authorization", $"Bearer {appConfig.ChapaSecretKey}");
+
+      string txRf = PaymentHelper.GetTransactionReference();
+
+      var restRequest = new RestRequest(
+        $"{appConfig.ChapaApiOrigin}/v1/charges?type={chapaCharge.PaymentMethod.GetDisplayName()}"
+      )
+      {
+        Method = Method.Post
+      };
+      restRequest.AddHeader("Content-Type", "application/json");
+      restRequest.AddBody(
+        new
+        {
+          amount = chapaCharge.Amount,
+          currency = chapaCharge.Currency.ToString(),
+          mobile = chapaCharge.PhoneNumber,
+          tx_rf = txRf
+        }
+      );
+
+      var response = await restClient.ExecuteAsync(restRequest);
+
+      if (!response.IsSuccessStatusCode)
+      {
+        throw new Exception(response.ErrorMessage);
+      }
+      var content = JsonSerializer.Deserialize<JsonElement>(response.Content ?? "");
+      if (
+        content.TryGetProperty("data", out var dataElement)
+        && dataElement.TryGetProperty("meta", out var meta)
+      )
+      {
+        return new ChargeResponse
+        {
+          Message = meta.TryGetProperty("message", out var messageProp)
+            ? messageProp.GetString() ?? ""
+            : "Message not available",
+          RefId = meta.TryGetProperty("ref_id", out var refId)
+            ? refId.GetString() ?? ""
+            : "RefId not available",
+          Status = meta.TryGetProperty("status", out var statusProp)
+            ? statusProp.GetString() == "success"
+            : false
+        };
+      }
+      else
+      {
+        throw new InvalidOperationException("Data or Meta object is missing in the response");
+      }
+    }
+    catch (System.Exception ex)
+    {
+      logger.LogError(ex, "An error occured tryingto charge in chapa");
+      throw;
+    }
+  }
+
+  public async Task<IVerifyResponse> VerifyAsync(IVerifyRequest verifyRequest)
+  {
+    try
+    {
+      // https://api.chapa.co/v1/transaction/verify/chewatatest-6669
+      var restClient = new RestClient();
+      restClient.AddDefaultHeader("Authorization", $"Bearer {appConfig.ChapaSecretKey}");
+
+      var restRequest = new RestRequest(
+        $"{appConfig.ChapaApiOrigin}/v1/transaction/verify/{verifyRequest.TransactionReference}"
+      )
+      {
+        Method = Method.Get
+      };
+
+      var response = await restClient.ExecuteAsync(restRequest);
+
+      if (!response.IsSuccessStatusCode)
+      {
+        throw new Exception(response.ErrorMessage);
+      }
+
+      var content = JsonSerializer.Deserialize<JsonElement>(response.Content ?? "");
+
+      return new VerifyResponse
+      {
+        Success = true,
+        FirstName =
+          content.TryGetProperty("data", out var data)
+          && data.TryGetProperty("first_name", out var firstName)
+            ? firstName.ToString()
+            : "",
+        LastName =
+          !data.IsNull() && data.TryGetProperty("last_name", out var lastName)
+            ? lastName.ToString()
+            : "",
+        Email =
+          !data.IsNull() && data.TryGetProperty("last_name", out var email) ? email.ToString() : "",
+      };
+    }
+    catch (System.Exception ex)
+    {
+      logger.LogError(ex, "An error occured trying to verify in chapa");
       throw;
     }
   }
