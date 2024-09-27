7c44411802476ec4e044361b9ef80fe01178158c
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-09-27 13:29:16 +0300

    Conversation Memberships navigation in Conversation for easy query

--- Source/Models/Entities/ConversationModel.cs
+++ Source/Models/Entities/ConversationModel.cs
@@ -6,4 +6,6 @@ public class Conversation : BaseEntity
 {
   public Guid ConversationId { get; set; } = Guid.NewGuid(); // <<PK>>
   public virtual ICollection<Message> Messages { get; set; } = new HashSet<Message>();
+  public virtual ICollection<ConversationMembership> ConversationMemberships { get; set; } =
+    new HashSet<ConversationMembership>();
 }
