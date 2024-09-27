47347d600a807dc1a0302977c9b5b7f04b34609b
Dagmawi Wegayehu <dagmawi.wegayehu-ug@aau.edu.et>
2024-09-27 13:31:07 +0300

    Chapa allowed Payment Methods

---
+++ Source/Models/Interfaces/Payments/Chapa/ChapaPaymentMethod.cs
@@ -0,0 +1,13 @@
+namespace HealthHub.Source.Models.Interfaces.Payments.Chapa;
+
+public enum ChapaPaymentMethod
+{
+  Unknown = 0,
+  TeleBirr,
+  CbeBirr,
+  // Below are currently not supported by chapa
+  // Mpesa,
+  // AwashBirr,
+  // Ebirr,
+  // Amole
+}
